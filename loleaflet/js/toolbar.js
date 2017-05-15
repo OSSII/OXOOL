ed79e8324abc4b15cc3f88ea9980ff4cd5f84db9
wind <yc.yan@ossii.com.tw>
2017-05-15 13:33:09 +0800

    mts 1011: loleaflet: 文件的字體若沒裝在系統內，
    則字型大小列表會消失，
    
    現在改為加上預設大小列表。

--- loleaflet/js/toolbar.js
+++ loleaflet/js/toolbar.js
@@ -1232,6 +1232,8 @@ function updateFontSizeList(font) {
 	$('.fontsizes-select').find('option').remove();
 	var data = [''];
 	data = data.concat(map.getToolbarCommandValues('.uno:CharFontName')[font]);
+	if (data[1] === undefined)
+		data = ['', 6,7,8,9,10,10.5,11,12,13,14,15,16,18,20,22,24,26,28,32,36,40,44,48,54,60,66,72,80,88,96];
 	$('.fontsizes-select').select2({
 		data: data,
 		placeholder: ' ',
@@ -1592,9 +1594,12 @@ function onCommandStateChanged(e) {
 		});
 		if (!found) {
 			// we need to add the size
-			$('.fontsizes-select')
-				.append($('<option></option>')
-				.text(state).val(state));
+			var defsizes = [6,7,8,9,10,10.5,11,12,13,14,15,16,18,20,22,24,26,28,32,36,40,44,48,54,60,66,72,80,88,96];
+			for (var si in defsizes)
+				$('.fontsizes-select')
+					.append($('<option></option>')
+					.text(defsizes[si]).val(defsizes[si]));
+
 		}
 		fontsizesSelectValue = state;
 		$('.fontsizes-select').val(state).trigger('change');
