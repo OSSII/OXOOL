92fe64b7e7fd45898918a454b7fb88a61d4f0b63
YiiChang Yen <sadwind.yan@gmail.com>
2019-02-15 10:26:15 +0800

    loleaflet: libreoffice 6-1-4: 修正傳回之 dialog 的比例(dpiscale)

--- loleaflet/src/control/Control.LokDialog.js
+++ loleaflet/src/control/Control.LokDialog.js
@@ -68,6 +68,7 @@ L.Control.LokDialog = L.Control.extend({
 
 		// pre-multiplied by the scale factor
 		var dpiscale = L.getDpiScaleFactor();
+		dpiscale = 1;
 		return [x * dpiscale, y * dpiscale, width * dpiscale, height * dpiscale].join(',');
 	},
 
@@ -76,6 +77,7 @@ L.Control.LokDialog = L.Control.extend({
 			rectangle = rectangle.replace(/ /g, '');
 
 		var dpiscale = L.getDpiScaleFactor();
+		dpiscale = 1;
 		console.log('_sendPaintWindow: rectangle: ' + rectangle + ', dpiscale: ' + dpiscale);
 		this._map._socket.sendMessage('paintwindow ' + id + (rectangle ? ' rectangle=' + rectangle + ' dpiscale=' + dpiscale : ''));
 	},
@@ -250,6 +252,7 @@ L.Control.LokDialog = L.Control.extend({
 		L.DomUtil.setStyle(canvas, 'height', height + 'px');
 
 		var scale = L.getDpiScaleFactor();
+		scale = 1;
 		canvas.width = width * scale;
 		canvas.height = height * scale;
 	},
