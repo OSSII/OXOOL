7cc97803d05da540f4c2d32ef8c5f293d0cc0054
Szymon Kłos <szymon.klos@collabora.com>
2019-03-19 10:07:50 +0100

    Escape username
    In case of guest users it was possible to inject html.
    
    Change-Id: I642de3efa0fa03cd2a8d63834605f46eacd0f464
    Reviewed-on: https://gerrit.libreoffice.org/69410
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Tested-by: Szymon Kłos <szymon.klos@collabora.com>
    (cherry picked from commit 3084565981d85d5734436c3411266c529ad5d879)
    (cherry picked from commit 7176214de3177ad3ecc2f79871cca686e2683ea3)
    Reviewed-on: https://gerrit.libreoffice.org/69422
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    Tested-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- loleaflet/js/toolbar.js
+++ loleaflet/js/toolbar.js
@@ -2153,11 +2153,16 @@ function updateUserListCount() {
 	$('#zoomlevel').html(zoomlevel);
 }
 
+function escapeHtml(input) {
+	return $('<div>').text(input).html();
+}
+
 function onAddView(e) {
+	var username = escapeHtml(e.username);
 	$('#tb_toolbar-down_item_userlist')
 		.w2overlay({
 			class: 'loleaflet-font',
-			html: userJoinedPopupMessage.replace('%user', e.username),
+			html: userJoinedPopupMessage.replace('%user', username),
 			style: 'padding: 5px'
 		});
 	clearTimeout(userPopupTimeout);
@@ -2167,7 +2172,6 @@ function onAddView(e) {
 		userPopupTimeout = null;
 	}, 3000);
 
-	var username = e.username;
 	var color = L.LOUtil.rgbToHex(map.getViewColor(e.viewId));
 	if (e.viewId === map._docLayer._viewId) {
 		username = _('You');
