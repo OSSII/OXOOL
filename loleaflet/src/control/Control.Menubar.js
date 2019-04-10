d5589e74f91e43e87ffd7458c98f5c2ee792e34c
Firefly <firefly@localhost.localdomain>
2019-04-10 14:13:26 +0800

    把子選單箭頭符號，改為U+23f5

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -553,7 +553,7 @@ L.Control.Menubar = L.Control.extend({
 			showTimeout: 0,
 			collapsibleHideDuration: 0,
 			subIndicatorsPos: 'append',
-			subIndicatorsText: '&#8250;'
+			subIndicatorsText: '&#x23f5;'
 		});
 		$('#main-menu').attr('tabindex', 0);
 
