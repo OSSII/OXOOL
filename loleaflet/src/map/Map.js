38fbe7770939189dd1d7379d09f350e61835ec39
Chiaming Teng <firefly@ossii.com.tw>
2019-08-13 11:16:57 +0800

    修正權限由 readonly 或 view 變更回 edit 狀態時， 部份的 readonly 屬性並未移除的問題

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -137,6 +137,14 @@ L.Map = L.Evented.extend({
 				L.DomUtil.addClass(L.DomUtil.get('main-menu'), 'readonly');
 				L.DomUtil.addClass(L.DomUtil.get('presentation-controls-wrapper'), 'readonly');
 				L.DomUtil.addClass(L.DomUtil.get('spreadsheet-row-column-frame'), 'readonly');
+			} else {
+				L.DomUtil.removeClass(this._container.parentElement, 'readonly');
+				if (!L.Browser.mobile) {
+					L.DomUtil.removeClass(L.DomUtil.get('toolbar-wrapper'), 'readonly');
+				}
+				L.DomUtil.removeClass(L.DomUtil.get('main-menu'), 'readonly');
+				L.DomUtil.removeClass(L.DomUtil.get('presentation-controls-wrapper'), 'readonly');
+				L.DomUtil.removeClass(L.DomUtil.get('spreadsheet-row-column-frame'), 'readonly');
 			}
 		}, this);
 		this.on('doclayerinit', function() {
