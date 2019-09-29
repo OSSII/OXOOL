201ca75a6b6e415f08deb6cf49947f9f23d5c380
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-09-29 17:00:56 -0400

    wsd: logging: color trace differently to improve readability
    Change-Id: I5127061f4d62122d07bad32d4757569f1a9cc877
    Reviewed-on: https://gerrit.libreoffice.org/80321
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Jan Holesovsky <kendy@collabora.com>

--- common/Log.cpp
+++ common/Log.cpp
@@ -154,7 +154,10 @@ namespace Log
             }
         }
         else if (withColor)
+        {
             channel = static_cast<Poco::Channel*>(new Poco::ColorConsoleChannel());
+            channel->setProperty("traceColor", "green");
+        }
         else
             channel = static_cast<Poco::Channel*>(new Poco::ConsoleChannel());
 
