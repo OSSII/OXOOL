62ce5586d6a62622a0dd6b7b7cdd61466e41110f
Chiaming Teng <firefly@ossii.com.tw>
2019-11-13 17:04:15 +0800

    正式切割出開發用套件初版：
    1、把 common、net、wsd、kit 和 LibreOfficeKit 目錄下所有的 header 抽出。
    2、把 common、net 程式，build in to liboxool.so 中，作為 share library 用。
    3. oxoolwsd 和 oxoolforkit 分別 build 兩種版本，提供 make run 及打包安裝用。

--- startwsd.sh
+++ startwsd.sh
@@ -2,6 +2,7 @@
 thisPath=`pwd`
 while true ; do
     LOOL_SERVE_FROM_FS=1 ./oxoolwsd --o:sys_template_path="$thisPath/systemplate" \
+	--config-file="$thisPath/oxoolwsd.xml" \
 	--o:lo_template_path="/opt/oxoffice" \
 	--o:child_root_path="$thisPath/jails" \
 	--o:storage.filesystem[@allow]=true \
