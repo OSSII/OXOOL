ac4e9a022b7346ce37b78b47794a8665808bb044
Firefly <firefly@ossii.com.tw>
2020-03-27 14:15:45 +0800

    關閉按鈕預設改為不顯示

--- loleaflet/js/toolbar.js
+++ loleaflet/js/toolbar.js
@@ -2897,7 +2897,7 @@ $(document).ready(function() {
 	if (!closebutton) {
 		$('#closebuttonwrapper').hide();
 	} else if (closebutton && !L.Browser.mobile) {
-		$('.closebuttonimage').show();
+		$('#closebuttonwrapper').show();
 	}
 
 	$('#closebutton').click(function() {
