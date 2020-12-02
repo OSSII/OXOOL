ea158f9a51b257e4eaf53327b260643cce5339e4
Firefly <firefly@ossii.com.tw>
2020-12-02 15:29:00 +0800

    修正使用者名稱中，若帶有 '%' 符號，會造成 load exception 的問題

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -1373,9 +1373,7 @@ private:
         {
             Object::Ptr authorObj = new Object();
             authorObj->set("type", "string");
-            std::string decodedUserName;
-            URI::decode(userName, decodedUserName);
-            authorObj->set("value", decodedUserName);
+            authorObj->set("value", userName);
             renderOptsObj->set(".uno:Author", authorObj);
         }
 
