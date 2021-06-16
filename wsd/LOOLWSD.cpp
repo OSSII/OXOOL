05ca7f662115c54dd34b5d0a084f89e7f92c907b
Firefly <firefly@ossii.com.tw>
2021-06-16 11:47:26 +0800

    oxoolwsd.xml.in 新增 client_port_number 指定 port

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -960,6 +960,7 @@ void LOOLWSD::initialize(Application& self)
             { "security.capabilities", "true" },
             { "security.seccomp", "true" },
             { "server_name", "" },
+            { "client_port_number", "9980" },
             { "ssl.ca_file_path", LOOLWSD_CONFIGDIR "/ca-chain.cert.pem" },
             { "ssl.cert_file_path", LOOLWSD_CONFIGDIR "/cert.pem" },
             { "ssl.enable", "true" },
@@ -1077,6 +1078,9 @@ void LOOLWSD::initialize(Application& self)
 
     ServerName = config().getString("server_name");
     LOG_INF("Initializing oxoolwsd server [" << ServerName << "].");
+
+    ClientPortNumber = config().getUInt("client_port_number", DEFAULT_CLIENT_PORT_NUMBER);
+    LOG_INF("Initializing oxoolwsd client port : " << ClientPortNumber);
 
     // Get anonymization settings.
 #if LOOLWSD_ANONYMIZE_USER_DATA
