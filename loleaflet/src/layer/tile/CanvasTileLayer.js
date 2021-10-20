6d369dc52254d4127779a26c342cedfdd5572df6
Henry Castro <hcastro@collabora.com>
2021-10-20 14:48:40 -0400

    loleaflet: move shouldWelcome function
    Change-Id: Ib246a86f79220c643dcd85bb811448fcac91e7ff
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -5286,9 +5286,6 @@ L.CanvasTileLayer = L.Layer.extend({
 					if (!isAnyVexDialogActive())
 						this._onCellCursorShift(true);
 				}
-				if (e.statusType === 'alltilesloaded' && this._map.shouldWelcome()) {
-					this._map.showWelcomeDialog();
-				}
 			},
 			this);
 
