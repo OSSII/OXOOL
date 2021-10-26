eaaaed260deb3354e908516e00f104992a2655e0
Firefly <firefly@ossii.com.tw>
2021-10-26 14:34:09 +0800

    admin: 管理系統引用部份 前台的 javascript libraries.
    1. jqiery smartmenus
    2. jquery UI
    3. unocommands.js
    4. L.Util
    5. L.DomUtil

--- loleaflet/admin/main-admin.js
+++ loleaflet/admin/main-admin.js
@@ -2,6 +2,9 @@
 // CSS requires
 require('vex-js/css/vex.css');
 require('vex-js/css/vex-theme-default.css');
+require('smartmenus/dist/css/sm-core-css.css');
+require('smartmenus/dist/css/sm-simple/sm-simple.css');
+require('jquery-ui/jquery-ui.css');
 
 var $ = require('jquery');
 global.$ = global.jQuery = $;
@@ -18,7 +21,14 @@ global._ = function (string) {
 	return string.toLocaleString();
 };
 
+global.L = {};
+
 global.l10nstrings = require('./admin.strings.js');
+require('smartmenus');
+require('jquery-ui/jquery-ui.js');
+require('../src/unocommands.js');
+require('../src/core/Util.js')
+require('../src/dom/DomUtil.js')
 
 global.d3 = require('d3');
 global.Admin = require('admin-src.js');
