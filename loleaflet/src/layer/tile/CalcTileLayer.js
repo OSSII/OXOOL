febbe2727ebf786887725077d3dcc3cb5c3414f9
Dennis Francis <dennis.francis@collabora.com>
2021-11-15 14:22:10 +0530

    calcRTL: RTL adjustments for column header
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: Id2400a62a845982b9896fb7fdb99a83819c47118

--- loleaflet/src/layer/tile/CalcTileLayer.js
+++ loleaflet/src/layer/tile/CalcTileLayer.js
@@ -631,6 +631,10 @@ L.CalcTileLayer = L.CanvasTileLayer.extend({
 		}
 	},
 
+	isLayoutRTL: function () {
+		return !!this._layoutIsRTL;
+	},
+
 	_handleSheetGeometryDataMsg: function (jsonMsgObj) {
 		if (!this.sheetGeometry) {
 			this._sheetGeomFirstWait = false;
