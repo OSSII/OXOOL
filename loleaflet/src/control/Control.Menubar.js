254ad2d3bf9ba12e44035cc75acec2c11187eb38
Pedro Pinto Silva <pedro.silva@collabora.com>
2022-04-04 16:41:20 +0200

    Mobile: Fix save icon and back icon overlay
    Save icon (blue tick) should not be visible at all when the hamburger
    menu is open
    
    On Calc and it is also needed to hide formularbar while hamburger menu
    is open because otherwise the it affects the size of the toolbar-wrapper
    and consequently affect the flex box and how the hamburger menu gets
    aligned
    
    before: https://archive.org/download/cool-bug-mobile-save-above-back/cool-bug-mobile-save-above-back.png
    
    Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
    Change-Id: Ifa5406a5a5c31172cde82e843949671fa95c6596

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -601,6 +601,8 @@ L.Control.Menubar = L.Control.extend({
 							self._map.fire('mobilewizard', {data: menuData});
 							$('#toolbar-hamburger').removeClass('menuwizard-closed').addClass('menuwizard-opened');
 							$('#mobile-wizard-header').hide();
+							$('#toolbar-mobile-back').hide();
+							$('#formulabar').hide();
 						}
 					} else if (!window.mode.isMobile()) {
 						// Ditto.
@@ -611,6 +613,8 @@ L.Control.Menubar = L.Control.extend({
 						window.mobileMenuWizard = false;
 						self._map.fire('closemobilewizard');
 						$('#toolbar-hamburger').removeClass('menuwizard-opened').addClass('menuwizard-closed');
+						$('#toolbar-mobile-back').show();
+						$('#formulabar').show();
 					}
 				});
 				// hide mobile menu beforeunload
