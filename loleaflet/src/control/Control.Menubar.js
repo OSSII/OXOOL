ad7f278128234f5dcca43c510a043e284f6febc7
Mert Tumer <mert.tumer@collabora.com>
2022-04-04 18:49:18 +0300

    mobile: hide send feedback button
    Signed-off-by: Mert Tumer <mert.tumer@collabora.com>
    Change-Id: Ic4ee06d419ffcb13916ef74327366c57ef87193a

--- loleaflet/src/control/Control.Menubar.js
+++ loleaflet/src/control/Control.Menubar.js
@@ -1547,6 +1547,9 @@ L.Control.Menubar = L.Control.extend({
 			}
 		}
 
+		if (item.id === 'feedback' && !this._map.feedback)
+			return undefined;
+
 		var itemName;
 		if (item.name)
 			itemName = item.name;
