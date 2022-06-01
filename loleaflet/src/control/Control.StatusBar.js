CollaboraOnline/online

Status bar: search: move inline styles to CSS file
Signed-off-by: Pedro Pinto Silva <pedro.silva@collabora.com>
Change-Id: I3165ed4ce504845a0dd9e3876b03260766d63a75

browser/src/control/Control.StatusBar.js
@@ -168,7 +168,7 @@ L.Control.StatusBar = L.Control.extend({
 				name: 'actionbar',
 				items: [
 					{type: 'html',  id: 'search',
-						html: '<div style="padding: 3px 5px 4px 10px;" class="cool-font">' +
+						html: '<div class="cool-font">' +
 					'<label for="search-input" class="visuallyhidden" aria-hidden="false">Search:</label>' +
 					'<input size="15" id="search-input" placeholder="' + _('Search') + '"' +
 					'style="padding: 3px; border-radius: 2px; border: 1px solid silver"/>' +

OSSII/OXOOL

loleaflet/src/control/Control.StatusBar.js
@@ -135,7 +135,7 @@ L.Control.StatusBar = L.Control.extend({
 				name: 'actionbar',
 				items: [
 					{type: 'html',  id: 'search',
-						html: '<div style="padding: 3px 5px 4px 10px;" class="oxool-font">' +
+						html: '<div class="oxool-font">' +
 					'<label for="search-input" class="visuallyhidden" aria-hidden="false">Search:</label>' +
 					'<input size="15" id="search-input" placeholder="' + _('Search') + '"' +
 					'style="padding: 3px; border-radius: 2px; border: 1px solid silver"/>' +