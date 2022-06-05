CollaboraOnline/online

msguniq after appending welcome strings
Signed-off-by: Andras Timar <andras.timar@collabora.com>
Change-Id: Ic4ceefca9d4084909b2f783738838bc340ac9e1a

browser/util/appendwelcome.py
@@ -19,4 +19,5 @@ for entry in welcome:
     if entry.msgid == '':
         continue
     coolui.append(entry)
+coolui.metadata['Content-Type'] = 'text/plain; charset=UTF-8'
 coolui.save(sys.argv[2])

OSSII/OXOOL

loleaflet/util/appendwelcome.py
@@ -19,4 +19,5 @@ for entry in welcome:
     if entry.msgid == '':
         continue
     oxoolui.append(entry)
+oxoolui.metadata['Content-Type'] = 'text/plain; charset=UTF-8'
 oxoolui.save(sys.argv[2])