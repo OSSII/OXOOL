CollaboraOnline/online

Welcome dialog fallback l10n
Signed-off-by: Andras Timar <andras.timar@collabora.com>
Change-Id: I4267481c5b34f0493ca8a7b1b50d398299deeae3

browser/util/appendwelcome.py
@@ -1,0 +1,22 @@
+#!/usr/bin/env python3
+"""Append Welcome dialog strings to COOL UI pot file"""
+
+import sys
+import polib
+
+welcome = polib.pofile(sys.argv[1],
+                  autodetect_encoding=False,
+                  encoding="utf-8",
+                  wrapwidth=-1)
+coolui = polib.pofile(sys.argv[2],
+                  autodetect_encoding=False,
+                  encoding="utf-8",
+                  wrapwidth=78)
+# Filter out unnecessary strings of meta tags from html2po output
+for entry in welcome:
+    if 'html.head.meta' in entry.occurrences[0][0]:
+        continue
+    if entry.msgid == '':
+        continue
+    coolui.append(entry)
+coolui.save(sys.argv[2])

OSSII/OXOOL

loleaflet/util/appendwelcome.py
@@ -1,0 +1,22 @@
+#!/usr/bin/env python3
+"""Append Welcome dialog strings to OXOOL UI pot file"""
+
+import sys
+import polib
+
+welcome = polib.pofile(sys.argv[1],
+                  autodetect_encoding=False,
+                  encoding="utf-8",
+                  wrapwidth=-1)
+oxoolui = polib.pofile(sys.argv[2],
+                  autodetect_encoding=False,
+                  encoding="utf-8",
+                  wrapwidth=78)
+# Filter out unnecessary strings of meta tags from html2po output
+for entry in welcome:
+    if 'html.head.meta' in entry.occurrences[0][0]:
+        continue
+    if entry.msgid == '':
+        continue
+    oxoolui.append(entry)
+oxoolui.save(sys.argv[2])