CollaboraOnline/online

welcome: fix translation of string with version number variable
Signed-off-by: Andras Timar <andras.timar@collabora.com>
Change-Id: Ia8d5e89b15017c68fe18ea88b5f29dc79de2e06c

browser/src/map/handler/Map.Welcome.js
@@ -85,7 +85,7 @@ L.Map.Welcome = L.Handler.extend({
 			this._iframeWelcome.clearTimeout();
 			var keys = Object.keys(data.strings);
 			for (var it in keys) {
-				data.strings[keys[it]] = _(keys[it]);
+				data.strings[keys[it]] = _(keys[it]).replace('%coolVersion', window.coolwsdVersion);
 			}
 			this._iframeWelcome.postMessage(data);
 		} else if (data.MessageId === 'welcome-close') {

OSSII/OXOOL

loleaflet/src/map/handler/Map.Welcome.js
@@ -85,7 +85,7 @@ L.Map.Welcome = L.Handler.extend({
 			this._iframeWelcome.clearTimeout();
 			var keys = Object.keys(data.strings);
 			for (var it in keys) {
-				data.strings[keys[it]] = _(keys[it]);
+				data.strings[keys[it]] = _(keys[it]).replace('%oxoolVersion', window.oxoolwsdVersion);
 			}
 			this._iframeWelcome.postMessage(data);
 		} else if (data.MessageId === 'welcome-close') {