3c72078da4a7dcf31191f59f97793bd67440753c
Firefly <firefly@ossii.com.tw>
2022-06-05 20:05:14 +0800

    l10n: if English variant is not specified then choose US as default

--- common/Session.cpp
+++ common/Session.cpp
@@ -163,7 +163,10 @@ void Session::parseDocOptions(const StringVector& tokens, int& part, std::string
         }
         else if (name == "lang")
         {
-            _lang = value;
+            if (value == "en")
+                _lang = "en-US";
+            else
+                _lang = value;
             ++offset;
         }
         else if (name == "watermarkWhenEditing")
