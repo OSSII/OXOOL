CollaboraOnline/online

enable eslint for CDarkOverlay.ts
* Use unix line endings.
* Use tabs instead of spaces to indent.
* Remove eslint-disable flag.

Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
Change-Id: Ic7fe77097c0e1b652636e5771fd4835d547ffa90

browser/src/layer/vector/CDarkOverlay.ts
@@ -1,67 +1,65 @@
-/* eslint-disable */
-
 /*
  * CDarkOverlay is used to render a dark overlay around an OLE object when selected
  */
 
 import Bounds = cool.Bounds;
 
-class CDarkOverlay extends CPathGroup{
+class CDarkOverlay extends CPathGroup {
 
-    private rectangles: CRectangle[] = [];
-    private options: any;
+	private rectangles: CRectangle[] = [];
+	private options: any;
 
-    constructor(pointSet: CPointSet, options: any) {
-        super([]);
-        this.options = options;
-        this.rectangles = this.createRectangles(4);
-        this.setPointSet(pointSet);
-    }
+	constructor(pointSet: CPointSet, options: any) {
+		super([]);
+		this.options = options;
+		this.rectangles = this.createRectangles(4);
+		this.setPointSet(pointSet);
+	}
 
-    private setPointSet(pointSet: CPointSet) {
-        var points = pointSet.getPointArray();
-        if (!points) {
-            for (var i = 0; i < this.rectangles.length; i++){
-                this.rectangles[i].setBounds(
-                    new cool.Bounds(new cool.Point(0, 0), new cool.Point(0, 1)));
-                this.push(this.rectangles[i]);
-            }
-            return;
-        }
-        
-        var rectangleBounds = this.invertOleBounds(new cool.Bounds(points[0], points[2]));
+	private setPointSet(pointSet: CPointSet) {
+		var points = pointSet.getPointArray();
+		if (!points) {
+			for (var i = 0; i < this.rectangles.length; i++) {
+				this.rectangles[i].setBounds(
+					new cool.Bounds(new cool.Point(0, 0), new cool.Point(0, 1)));
+				this.push(this.rectangles[i]);
+			}
+			return;
+		}
 
-        for (var i = 0; i < this.rectangles.length; i++){
-            this.rectangles[i].setBounds(rectangleBounds[i]);
-            this.push(this.rectangles[i]);
-        }
-    }
+		var rectangleBounds = this.invertOleBounds(new cool.Bounds(points[0], points[2]));
 
-    private invertOleBounds(oleBounds: cool.Bounds): cool.Bounds[] {
-        var rectanglesBounds: cool.Bounds[] = [];
+		for (var i = 0; i < this.rectangles.length; i++) {
+			this.rectangles[i].setBounds(rectangleBounds[i]);
+			this.push(this.rectangles[i]);
+		}
+	}
 
-        var minWidth = 0;
-        var minHeight = 0;
-        var fullWidth = 1000000;
-        var fullHeight = 1000000;
+	private invertOleBounds(oleBounds: cool.Bounds): cool.Bounds[] {
+		var rectanglesBounds: cool.Bounds[] = [];
 
-        rectanglesBounds.push(new cool.Bounds(new cool.Point(minWidth, minHeight), new cool.Point(fullWidth, oleBounds.min.y)));
-        rectanglesBounds.push(new cool.Bounds(new cool.Point(minWidth, oleBounds.min.y), oleBounds.getBottomLeft()));
-        rectanglesBounds.push(new cool.Bounds(oleBounds.getTopRight(), new cool.Point(fullWidth, oleBounds.max.y)));
-        rectanglesBounds.push(new cool.Bounds(new cool.Point(minWidth, oleBounds.max.y), new cool.Point(fullWidth, fullHeight)));
+		var minWidth = 0;
+		var minHeight = 0;
+		var fullWidth = 1000000;
+		var fullHeight = 1000000;
 
-        return rectanglesBounds;
-    }
+		rectanglesBounds.push(new cool.Bounds(new cool.Point(minWidth, minHeight), new cool.Point(fullWidth, oleBounds.min.y)));
+		rectanglesBounds.push(new cool.Bounds(new cool.Point(minWidth, oleBounds.min.y), oleBounds.getBottomLeft()));
+		rectanglesBounds.push(new cool.Bounds(oleBounds.getTopRight(), new cool.Point(fullWidth, oleBounds.max.y)));
+		rectanglesBounds.push(new cool.Bounds(new cool.Point(minWidth, oleBounds.max.y), new cool.Point(fullWidth, fullHeight)));
 
-    private createRectangles(quantity : number): CRectangle[] {
-        var rectangles: CRectangle[] = [];
-        for (var i = 0; i < quantity; i++){
-            rectangles.push(
-                new CRectangle(new cool.Bounds(
-                    new cool.Point(0, 0), new cool.Point(0, 1)
-                ), this.options));
-        }
+		return rectanglesBounds;
+	}
 
-        return rectangles;
-    }
+	private createRectangles(quantity: number): CRectangle[] {
+		var rectangles: CRectangle[] = [];
+		for (var i = 0; i < quantity; i++) {
+			rectangles.push(
+				new CRectangle(new cool.Bounds(
+					new cool.Point(0, 0), new cool.Point(0, 1)
+				), this.options));
+		}
+
+		return rectangles;
+	}
 }

OSSII/OXOOL

loleaflet/src/layer/vector/CDarkOverlay.ts
@@ -1,67 +1,65 @@
-/* eslint-disable */
-
 /*
  * CDarkOverlay is used to render a dark overlay around an OLE object when selected
  */
 
 import Bounds = oxool.Bounds;
 
-class CDarkOverlay extends CPathGroup{
+class CDarkOverlay extends CPathGroup {
 
-    private rectangles: CRectangle[] = [];
-    private options: any;
+	private rectangles: CRectangle[] = [];
+	private options: any;
 
-    constructor(pointSet: CPointSet, options: any) {
-        super([]);
-        this.options = options;
-        this.rectangles = this.createRectangles(4);
-        this.setPointSet(pointSet);
-    }
+	constructor(pointSet: CPointSet, options: any) {
+		super([]);
+		this.options = options;
+		this.rectangles = this.createRectangles(4);
+		this.setPointSet(pointSet);
+	}
 
-    private setPointSet(pointSet: CPointSet) {
-        var points = pointSet.getPointArray();
-        if (!points) {
-            for (var i = 0; i < this.rectangles.length; i++){
-                this.rectangles[i].setBounds(
-                    new oxool.Bounds(new oxool.Point(0, 0), new oxool.Point(0, 1)));
-                this.push(this.rectangles[i]);
-            }
-            return;
-        }
-        
-        var rectangleBounds = this.invertOleBounds(new oxool.Bounds(points[0], points[2]));
+	private setPointSet(pointSet: CPointSet) {
+		var points = pointSet.getPointArray();
+		if (!points) {
+			for (var i = 0; i < this.rectangles.length; i++) {
+				this.rectangles[i].setBounds(
+					new oxool.Bounds(new oxool.Point(0, 0), new oxool.Point(0, 1)));
+				this.push(this.rectangles[i]);
+			}
+			return;
+		}
 
-        for (var i = 0; i < this.rectangles.length; i++){
-            this.rectangles[i].setBounds(rectangleBounds[i]);
-            this.push(this.rectangles[i]);
-        }
-    }
+		var rectangleBounds = this.invertOleBounds(new oxool.Bounds(points[0], points[2]));
 
-    private invertOleBounds(oleBounds: oxool.Bounds): oxool.Bounds[] {
-        var rectanglesBounds: oxool.Bounds[] = [];
+		for (var i = 0; i < this.rectangles.length; i++) {
+			this.rectangles[i].setBounds(rectangleBounds[i]);
+			this.push(this.rectangles[i]);
+		}
+	}
 
-        var minWidth = 0;
-        var minHeight = 0;
-        var fullWidth = 1000000;
-        var fullHeight = 1000000;
+	private invertOleBounds(oleBounds: oxool.Bounds): oxool.Bounds[] {
+		var rectanglesBounds: oxool.Bounds[] = [];
 
-        rectanglesBounds.push(new oxool.Bounds(new oxool.Point(minWidth, minHeight), new oxool.Point(fullWidth, oleBounds.min.y)));
-        rectanglesBounds.push(new oxool.Bounds(new oxool.Point(minWidth, oleBounds.min.y), oleBounds.getBottomLeft()));
-        rectanglesBounds.push(new oxool.Bounds(oleBounds.getTopRight(), new oxool.Point(fullWidth, oleBounds.max.y)));
-        rectanglesBounds.push(new oxool.Bounds(new oxool.Point(minWidth, oleBounds.max.y), new oxool.Point(fullWidth, fullHeight)));
+		var minWidth = 0;
+		var minHeight = 0;
+		var fullWidth = 1000000;
+		var fullHeight = 1000000;
 
-        return rectanglesBounds;
-    }
+		rectanglesBounds.push(new oxool.Bounds(new oxool.Point(minWidth, minHeight), new oxool.Point(fullWidth, oleBounds.min.y)));
+		rectanglesBounds.push(new oxool.Bounds(new oxool.Point(minWidth, oleBounds.min.y), oleBounds.getBottomLeft()));
+		rectanglesBounds.push(new oxool.Bounds(oleBounds.getTopRight(), new oxool.Point(fullWidth, oleBounds.max.y)));
+		rectanglesBounds.push(new oxool.Bounds(new oxool.Point(minWidth, oleBounds.max.y), new oxool.Point(fullWidth, fullHeight)));
 
-    private createRectangles(quantity : number): CRectangle[] {
-        var rectangles: CRectangle[] = [];
-        for (var i = 0; i < quantity; i++){
-            rectangles.push(
-                new CRectangle(new oxool.Bounds(
-                    new oxool.Point(0, 0), new oxool.Point(0, 1)
-                ), this.options));
-        }
+		return rectanglesBounds;
+	}
 
-        return rectangles;
-    }
+	private createRectangles(quantity: number): CRectangle[] {
+		var rectangles: CRectangle[] = [];
+		for (var i = 0; i < quantity; i++) {
+			rectangles.push(
+				new CRectangle(new oxool.Bounds(
+					new oxool.Point(0, 0), new oxool.Point(0, 1)
+				), this.options));
+		}
+
+		return rectangles;
+	}
 }