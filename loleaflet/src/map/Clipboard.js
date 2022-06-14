CollaboraOnline/online

ignore if comment is selected while pasting
if comment is selected but is not in editing,
proceed to normal pasting in document

problem:
while pasting content multiple times with comment,
previously pasted comment stays selected and can't further normally

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Ia594c725f4fe7108d6aea1591d88b2c306202be3

browser/src/map/Clipboard.js
@@ -515,7 +515,7 @@ L.Clipboard = L.Class.extend({
 		if (isAnyVexDialogActive() && !(this.pasteSpecialVex && this.pasteSpecialVex.isOpen))
 			return true;
 
-		if ($('.annotation-active').length)
+		if ($('.annotation-active').length && $('.cool-annotation-edit').is(':visible'))
 		    return true;
 
 		return false;

OSSII/OXOOL

loleaflet/src/map/Clipboard.js
@@ -515,7 +515,7 @@ L.Clipboard = L.Class.extend({
 		if (isAnyVexDialogActive() && !(this.pasteSpecialVex && this.pasteSpecialVex.isOpen))
 			return true;
 
-		if ($('.annotation-active').length)
+		if ($('.annotation-active').length && $('.oxool-annotation-edit').is(':visible'))
 		    return true;
 
 		return false;