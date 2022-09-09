2ceff98c90af29e48a9f434821e6215da2a76534
Firefly <firefly@ossii.com.tw>
2022-09-09 16:12:44 +0800

    修正 make 及 make run

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -909,7 +909,11 @@ void LOOLWSD::initialize(Application& self)
     }
 #endif
 
+#if ENABLE_DEBUG
+    Util::setApplicationPath(DEBUG_ABSSRCDIR);
+#else
     Util::setApplicationPath(Poco::Path(Application::instance().commandPath()).parent().toString());
+#endif
 
     if (!UnitWSD::init(UnitWSD::UnitType::Wsd, UnitTestLibrary))
     {
@@ -2031,7 +2035,11 @@ bool LOOLWSD::createForKit()
 #ifdef STRACE_LOOLFORKIT
     std::string forKitPath = "strace";
 #else
+#if ENABLE_DEBUG
+    std::string forKitPath = DEBUG_ABSSRCDIR "/oxoolforkit";
+#else
     std::string forKitPath = Path(Application::instance().commandPath()).parent().toString() + "oxoolforkit";
+#endif
 #endif
 
     // Always reap first, in case we haven't done so yet.
