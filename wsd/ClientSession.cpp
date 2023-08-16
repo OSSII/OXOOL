CollaboraOnline/online

Adding support for enabling/disabling a11y support from the ui
Added toggle button/menu entry for enable/disable accessibility support.
This ui feature is available for Online Writer only.
The button/menu entry is added only when accessibility is enabled at
server level.
That allows to enable/disable accessibility per view.
By default, the accessibility support is disabled.
Anyway the accessibility support state is saved to local storage
if available.

Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
Change-Id: If5968a47f17922038b9da3d320cbed84ebb7688b

wsd/ClientSession.cpp
@@ -992,6 +992,13 @@ bool ClientSession::_handleInput(const char *buffer, int length)
         }
         return true;
     }
+    else if (tokens.equals(0, "a11ystate"))
+    {
+        if (COOLWSD::getConfigValue<bool>("accessibility.enable", false))
+        {
+            return forwardToChild(std::string(buffer, length), docBroker);
+        }
+    }
     else if (tokens.equals(0, "completefunction"))
     {
         return forwardToChild(std::string(buffer, length), docBroker);
@@ -1189,10 +1196,9 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
             oss << " macroSecurityLevel=" << COOLWSD::getConfigValue<int>("security.macro_security_level", 1);
         }
 
-        if (COOLWSD::hasProperty("accessibility.enable"))
+        if (COOLWSD::getConfigValue<bool>("accessibility.enable", false))
         {
-            oss << " enableAccessibility=" << std::boolalpha
-                << COOLWSD::getConfigValue<bool>("accessibility.enable", false);
+            oss << " accessibilityState=" << std::boolalpha << getAccessibilityState();
         }
 
         if (!getDocOptions().empty())

OSSII/OXOOL

wsd/ClientSession.cpp
@@ -1002,6 +1002,13 @@ bool ClientSession::_handleInput(const char *buffer, int length)
         }
         return true;
     }
+    else if (tokens.equals(0, "a11ystate"))
+    {
+        if (OXOOLWSD::getConfigValue<bool>("accessibility.enable", false))
+        {
+            return forwardToChild(std::string(buffer, length), docBroker);
+        }
+    }
     else if (tokens.equals(0, "completefunction"))
     {
         return forwardToChild(std::string(buffer, length), docBroker);
@@ -1199,10 +1206,9 @@ bool ClientSession::loadDocument(const char* /*buffer*/, int /*length*/,
             oss << " macroSecurityLevel=" << OXOOLWSD::getConfigValue<int>("security.macro_security_level", 1);
         }
 
-        if (OXOOLWSD::hasProperty("accessibility.enable"))
+        if (OXOOLWSD::getConfigValue<bool>("accessibility.enable", false))
         {
-            oss << " enableAccessibility=" << std::boolalpha
-                << OXOOLWSD::getConfigValue<bool>("accessibility.enable", false);
+            oss << " accessibilityState=" << std::boolalpha << getAccessibilityState();
         }
 
         if (!getDocOptions().empty())