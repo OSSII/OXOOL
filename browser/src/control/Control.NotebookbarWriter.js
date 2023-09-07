e73655d2cab69165aa2f46c15ce9791864132d83
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2023-09-07 15:20:17 +0530

    Add QR and Bar code generation option
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I3fafb97e99df87f813562063192e6ae39b34c2d1

--- browser/src/control/Control.NotebookbarWriter.js
+++ browser/src/control/Control.NotebookbarWriter.js
@@ -1250,6 +1250,13 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'accessibility': { focusBack: false,	combination: 'FM',	de: null }
 			},
 			{
+				'id': 'insert-QrCode',
+				'type': 'bigtoolitem',
+				'text': _UNO('.uno:InsertQrCode', 'text'),
+				'command': '.uno:InsertQrCode',
+				'accessibility': { focusBack: false,	combination: 'IQ',	de: null }
+			},
+			{
 				'type': 'container',
 				'children': [
 					{
