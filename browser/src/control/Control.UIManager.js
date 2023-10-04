e3cc79a72b61ae9bbf9ee51ff93340f8ef4d63c4
Szymon Kłos <szymon.klos@collabora.com>
2023-10-04 19:41:42 +0200

    clipboard: use snackbar for interaction with user
    Instead of leaflet popup we use now snackbar.
    - control is not removed and added again for next copy-paste
      but we keep single instance for all the time
    - added download error message
    - simplified close snackbar code
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I804a82c4f589b029a42fc2800958ff2b46b7df50

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -921,18 +921,16 @@ L.Control.UIManager = L.Control.extend({
 
 	// Snack bar
 
+	closeSnackbar: function() {
+		var closeMessage = { id: 'snackbar', jsontype: 'dialog', type: 'snackbar', action: 'close' };
+		app.socket._onMessage({ textMsg: 'jsdialog: ' + JSON.stringify(closeMessage) });
+	},
+
 	showSnackbar: function(label, action, callback, timeout, hasProgress) {
 		if (!app.socket)
 			return;
 
-		var closeJson = {
-			id: 'snackbar',
-			jsontype: 'dialog',
-			type: 'snackbar',
-			action: 'fadeout'
-		};
-
-		app.socket._onMessage({textMsg: 'jsdialog: ' + JSON.stringify(closeJson)});
+		this.closeSnackbar();
 
 		var json = {
 			id: 'snackbar',
@@ -952,6 +950,7 @@ L.Control.UIManager = L.Control.extend({
 			]
 		};
 
+		var that = this;
 		var builderCallback = function(objectType, eventType, object, data) {
 			window.app.console.debug('control: \'' + objectType + '\' id:\'' + object.id + '\' event: \'' + eventType + '\' state: \'' + data + '\'');
 
@@ -959,8 +958,7 @@ L.Control.UIManager = L.Control.extend({
 				if (callback)
 					callback();
 
-				var closeMessage = { id: 'snackbar', jsontype: 'dialog', type: 'snackbar', action: 'close' };
-				app.socket._onMessage({ textMsg: 'jsdialog: ' + JSON.stringify(closeMessage) });
+				that.closeSnackbar();
 			}
 		};
 
