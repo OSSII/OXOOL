c34baac34fff574af41fdd15bd9715cf485d1b26
Szymon Kłos <szymon.klos@collabora.com>
2023-10-09 16:51:18 +0200

    clipboard: don't show full warning on paste
    if complex selection was copied in one window
    then we try to paste in other document, then
    the same dialog appeared. this commit shows
    only progressbar step for "paste" operation
    as we don't need to do anything else.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Id9c33261dbdfb282d142cdfdf2217b30111263b1

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -1186,7 +1186,7 @@ L.Control.UIManager = L.Control.extend({
 				vertical: false,
 				layoutstyle: 'end'
 			},
-		], responseButtonId);
+		], buttonText ? responseButtonId : cancelButtonId);
 
 		var that = this;
 		this.showModal(json, [
