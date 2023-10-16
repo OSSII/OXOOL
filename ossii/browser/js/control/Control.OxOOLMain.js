854db50335226e57bf801558a14a7b1a4f6e1598
Firefly <firefly@ossii.com.tw>
2023-10-16 15:10:10 +0800

    [ox] Introduce OSSII to strengthen the expansion mechanism.
    The OSSII enhancement and expansion mechanism is completely independent in the ossii directory.
    
    Signed-off-by: Firefly <firefly@ossii.com.tw>
    Change-Id: Ib15f52be8bf2769ace1e064fea72fa2d71b795d0

---
+++ ossii/browser/js/control/Control.OxOOLMain.js
@@ -0,0 +1,34 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+
+L.Control.OxOOLMain = L.Control.extend({
+    options: {},
+
+    onAdd: function (map) {
+        map.on('doclayerinit', this.onDocLayerInit, this);
+
+    },
+
+    onRemove: function() {
+        this.map.off('doclayerinit', this.onDocLayerInit, this);
+    },
+
+    onDocLayerInit: function(e) {
+        console.debug('OxOOLMain.onDocLayerInit', e);
+        var docType = this._map.getDocType();
+        // 取得文件類型的識別色
+        var docIdentify = getComputedStyle(document.documentElement).getPropertyValue('--' + docType + '-identify-color');
+        // 設定 CSS 變數 --doc-identify-color 爲目前文件的識別色
+        if (docIdentify) {
+            document.documentElement.style.setProperty('--doc-identify-color', docIdentify);
+            console.debug('document identify color:', docIdentify);
+        } else {
+            console.warn('Can not get document identify color: ' + docType);
+        }
+    }
+});
+
+L.control.OxOOLMain = function (options) {
+    return new L.Control.OxOOLMain(options);
+};
+
+/* vim: set ts=8 sts=8 sw=8 tw=100: */
---
+++ ossii/browser/js/main.js
@@ -0,0 +1,14 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+/* global L app */
+// 偷天換日
+L.Control.Menubar = L.Control.OxMenubar;
+
+(function() {
+    'use strict';
+
+    // register the main control
+    app.map.addControl(L.control.OxOOLMain());
+
+})();
+
+/* vim: set ts=8 sts=8 sw=8 tw=100: */
