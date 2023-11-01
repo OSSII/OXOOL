CollaboraOnline/online

Avoid logging messages when anonymization is on
Signed-off-by: Cosmin Humeniuc <cosmin.humeniuc@1and1.ro>
Change-Id: I5bcf615521d13758b2b70a0c9466a3bbc1a0a918

wsd/DocumentBroker.cpp
@@ -3658,7 +3658,8 @@ bool DocumentBroker::forwardToClient(const std::shared_ptr<Message>& payload)
             }
             else
             {
-                LOG_WRN("Client session [" << sid << "] not found to forward message: " << payload->abbr());
+                const std::string abbreviatedPayload = COOLWSD::AnonymizeUserData ? "..." : payload->abbr();
+                LOG_WRN("Client session [" << sid << "] not found to forward message: " << abbreviatedPayload);
             }
         }
     }

OSSII/OXOOL

wsd/DocumentBroker.cpp
@@ -3659,7 +3659,8 @@ bool DocumentBroker::forwardToClient(const std::shared_ptr<Message>& payload)
             }
             else
             {
-                LOG_WRN("Client session [" << sid << "] not found to forward message: " << payload->abbr());
+                const std::string abbreviatedPayload = OXOOLWSD::AnonymizeUserData ? "..." : payload->abbr();
+                LOG_WRN("Client session [" << sid << "] not found to forward message: " << abbreviatedPayload);
             }
         }
     }