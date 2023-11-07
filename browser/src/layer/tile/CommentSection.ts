1fc3cd95eaf9b15b5dc8925839b30d650de01fd2
Pranam Lashkari <lpranam@collabora.com>
2023-11-07 10:11:03 +0530

    annotation: fixed impress disappearing comments on autosave
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I8feba6907b2899a19dcda3ecf56c673801e27487

--- browser/src/layer/tile/CommentSection.ts
+++ browser/src/layer/tile/CommentSection.ts
@@ -631,7 +631,7 @@ export class Comment extends CanvasSectionObject {
 	}
 
 	private showMarker (): void {
-		if (this.sectionProperties.annotationMarker != null) {
+		if (this.sectionProperties.annotationMarker != null && !this.sectionProperties.isRemoved) {
 			this.map.addLayer(this.sectionProperties.annotationMarker);
 		}
 	}
