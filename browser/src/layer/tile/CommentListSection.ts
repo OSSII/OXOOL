0a2d08478037d7f96460c3437822f4f14c787a06
Pranam Lashkari <lpranam@collabora.com>
2023-11-21 22:33:05 +0530

    annotation: make sure parent-child relation is established
    problem:
    some times we order comments before parent-child relation is established,
    this caused reply count not being updated correctly for other users
    
    steps to reproduce:
    User A open ODT, let us say already with comment or add one, and keep Sidebar open so that comments are short.
    user B opens the same ODT with full comments. adds reply
    User A does not update ticket number, does not have +1
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: If3360c8dd938c6bd177764d3c1383d7f3f845990

--- browser/src/layer/tile/CommentListSection.ts
+++ browser/src/layer/tile/CommentListSection.ts
@@ -1043,6 +1043,7 @@ export class CommentSection extends CanvasSectionObject {
 
 		this.sectionProperties.commentList.push(annotation);
 
+		this.adjustParentAdd(annotation);
 		this.orderCommentList();
 		this.updateIdIndexMap();
 		this.checkSize();
@@ -1186,7 +1187,6 @@ export class CommentSection extends CanvasSectionObject {
 			} else {
 				this.adjustComment(obj.comment);
 				annotation = this.add(obj.comment);
-				this.adjustParentAdd(annotation);
 				if (this.sectionProperties.docLayer._docType === 'spreadsheet')
 					annotation.hide();
 			}
