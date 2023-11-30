12ef5e1ee78d2918e53283a664a049976a78aae5
Miklos Vajna <vmiklos@collabora.com>
2023-11-30 08:45:49 +0100

    doc broker: handle no _storage in uploadToStorageInternal()
    Similar to commit eb5c86a4d32d4e84a0e6dddaa6c4b5772435b85f
    (DocumentBroker::saveToStorage: guard against nullptr _storage,
    2020-10-26), with the same input.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I17ac2bc12ba086d16ccbf3d5c758e081a32cbf5a

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1719,7 +1719,11 @@ void DocumentBroker::uploadToStorageInternal(const std::shared_ptr<ClientSession
         Util::mapAnonymized(newFilename, fileId);
     }
 
-    assert(_storage && "Must have a valid Storage instance");
+    if (!_storage)
+    {
+        LOG_WRN("Expected to have a valid Storage instance, but doesn't have one");
+        return;
+    }
 
     const std::string uriAnonym = OXOOLWSD::anonymizeUrl(uri);
 
