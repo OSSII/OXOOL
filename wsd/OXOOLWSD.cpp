3f256bce151cf05171b70e0901b2414d1987df02
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-02-12 16:15:56 +0000

    disable getting the host printer queue
    and default printer. In typical deployment these should be effectively
    no no-op, so mostly aligns debugging env with deployment env so whatever
    printer happens to be installed locally has no effect.
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I09651d9af2de9817d4d278d25937c9ce0dc87352
    (cherry picked from commit 377c826894c6a70a7cb1add43822396edfc1d9a9)

--- wsd/OXOOLWSD.cpp
+++ wsd/OXOOLWSD.cpp
@@ -2834,6 +2834,9 @@ void OXOOLWSD::innerInitialize(Application& self)
 
     // Otherwise we profile the soft-device at jail creation time.
     setenv("SAL_DISABLE_OPENCL", "true", 1);
+    // Disable getting the OS print queue and default printer
+    setenv("SAL_DISABLE_PRINTERLIST", "true", 1);
+    setenv("SAL_DISABLE_DEFAULTPRINTER", "true", 1);
 
     // Log the connection and document limits.
 #if ENABLE_WELCOME_MESSAGE
