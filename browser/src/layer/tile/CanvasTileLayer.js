581962c9c2d7c14f305c95dbc75422b2a0d2148f
Neil Guertin <neil.guertin@collabora.com>
2024-02-13 15:27:06 -0500

    Clarify Automated User menu (backport to 23.05)
    Move Typer and Randomize User Settings to "Functionality" menu
    Rename main toggle to "Enable"
    
    Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
    Change-Id: I75709124f835c46ae741c4d51e14ed0b868528ae

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -5355,7 +5355,7 @@ L.CanvasTileLayer = L.Layer.extend({
 
 		this._addDebugTool({
 			name: 'Typer',
-			category: 'Automated User',
+			category: 'Functionality',
 			startsOn: false,
 			onAdd: function () {
 				self._debugLorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n';
@@ -5369,7 +5369,7 @@ L.CanvasTileLayer = L.Layer.extend({
 
 		this._addDebugTool({
 			name: 'Randomize user settings',
-			category: 'Automated User',
+			category: 'Functionality',
 			startsOn: !!window.oxoolParams.get('randomUser'),
 			onAdd: function () {
 				self._debugRandomizeSettings();
@@ -5379,7 +5379,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		});
 
 		this._addDebugTool({
-			name: 'Automated user input',
+			name: 'Enable automated user',
 			category: 'Automated User',
 			startsOn: false,
 			onAdd: function () {
