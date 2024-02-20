c0eba098f1b016af3d1cab73ad0b8e3b85780c21
Henry Castro <hcastro@collabora.com>
2024-02-20 16:23:37 -0400

    add parameter "theme" to load a document
    The UNO command is dispatched to change the theme when loading the
    document, it causes serious problems the "kit" process changes the
    canonical view id.
    
    So this patch avoids sending UNO command and add a parameter to set
    the initial theme.
    
    Change-Id: I523dda19cacd6fd3f81cd86ee554aacd5f73edea
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -147,7 +147,6 @@ L.Control.UIManager = L.Control.extend({
 		else {
 			this.loadLightMode();
 		}
-		this.activateDarkModeInCore(selectedMode);
 	},
 
 	activateDarkModeInCore: function(activate) {
