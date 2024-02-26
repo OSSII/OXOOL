Rash419/online

improve error message on invalid WOPI host
- before this patch when socket connection failed, we used to show the
  same error for socket connection failure and send the
  'Action_Load_Resp' with same errorMsg
- now we show/send different message for unauthorized and loading
  failure

Signed-off-by: Rash419 <rashesh.padia@collabora.com>
Change-Id: I71b1b7f8e2eca93790d068583370787825911a41

browser/src/core/Socket.js
@@ -1533,17 +1533,33 @@ app.definitions.Socket = L.Class.extend({
 		// Let onclose (_onSocketClose) report errors.
 	},
 
-	_onSocketClose: function () {
+	_onSocketClose: function (event) {
 		window.app.console.debug('_onSocketClose:');
 		if (!this._map._docLoadedOnce && this.ReconnectCount === 0) {
+			var errorMsg, errorType = '';
+			var reason = event.reason;
+			if (reason.startsWith('error:')) {
+				var command = this.parseServerCmd(reason);
+				if (command.errorCmd === 'internal' && command.errorKind === 'unauthorized') {
+					errorType = 'websocketunauthorized';
+					errorMsg = errorMessages.unauthorized;
+				} else if (command.errorCmd === 'storage' && command.errorKind === 'loadfailed') {
+					errorType = 'websocketloadfailed';
+					errorMsg = errorMessages.storage.loadfailed;
+				}
+			} else {
+				errorType = 'websocketconnectionfailed';
+				errorMsg = errorMessages.websocketconnectionfailed;
+			}
+			this._map.fire('error', { msg: errorMsg, cmd: 'socket', kind: 'closed', id: 4 });
 			var postMessageObj = {
-				errorType: 'websocketconnectionfailed',
+				errorType: errorType, 
 				success: false,
-				errorMsg: errorMessages.websocketconnectionfailed,
+				errorMsg: errorMsg,
 				result: '',
 			};
-			this._map.fire('postMessage', {msgId: 'Action_Load_Resp', args: postMessageObj});
-			this._map.fire('error', {msg: errorMessages.websocketconnectionfailed, cmd: 'socket', kind: 'closed', id: 4});
+			this._map.fire('postMessage', { msgId: 'Action_Load_Resp', args: postMessageObj });
+			return;
 		}
 		if (this.ReconnectCount > 0)
 			return;

OSSII/OXOOL

browser/src/core/Socket.js
@@ -1550,17 +1550,36 @@ app.definitions.Socket = L.Class.extend({
 		// Let onclose (_onSocketClose) report errors.
 	},
 
-	_onSocketClose: function () {
+	_onSocketClose: function (event) {
 		window.app.console.debug('_onSocketClose:');
 		if (!this._map._docLoadedOnce && this.ReconnectCount === 0) {
+			var errorMsg, errorType = '';
+			var reason = event.reason;
+			if (reason && reason.startsWith('error:')) {
+				var command = this.parseServerCmd(reason);
+				if (command.errorCmd === 'internal' && command.errorKind === 'unauthorized') {
+					errorType = 'websocketunauthorized';
+					errorMsg = errorMessages.unauthorized;
+				} else if (command.errorCmd === 'storage' && command.errorKind === 'loadfailed') {
+					errorType = 'websocketloadfailed';
+					errorMsg = errorMessages.storage.loadfailed;
+				} else {
+					errorType = 'websocketgenericfailure';
+					errorMsg = errorMessages.websocketgenericfailure;
+				}
+			} else {
+				errorType = 'websocketproxyfailure';
+				errorMsg = errorMessages.websocketproxyfailure;
+			}
+			this._map.fire('error', { msg: errorMsg, cmd: 'socket', kind: 'closed', id: 4 });
 			var postMessageObj = {
-				errorType: 'websocketconnectionfailed',
+				errorType: errorType,
 				success: false,
-				errorMsg: errorMessages.websocketconnectionfailed,
+				errorMsg: errorMsg,
 				result: '',
 			};
-			this._map.fire('postMessage', {msgId: 'Action_Load_Resp', args: postMessageObj});
-			this._map.fire('error', {msg: errorMessages.websocketconnectionfailed, cmd: 'socket', kind: 'closed', id: 4});
+			this._map.fire('postMessage', { msgId: 'Action_Load_Resp', args: postMessageObj });
+			return;
 		}
 		if (this.ReconnectCount > 0)
 			return;