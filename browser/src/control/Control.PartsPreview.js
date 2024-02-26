CollaboraOnline/online

annotation: don't change slide/sheet while editing comment
comments can not be committed when user is on different slide/sheet
then comments origin.

Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: I5c2a92da1a8e9788fcc3abc72f697873c7100993

browser/src/control/Control.PartsPreview.js
@@ -3,7 +3,7 @@
  * L.Control.PartsPreview
  */
 
-/* global _ app $ Hammer w2ui _UNO */
+/* global _ app $ Hammer w2ui _UNO cool */
 L.Control.PartsPreview = L.Control.extend({
 	options: {
 		fetchThumbnail: true,
@@ -499,6 +499,11 @@ L.Control.PartsPreview = L.Control.extend({
 	},
 
 	_setPart: function (e) {
+		if (cool.Comment.isAnyEdit()) {
+			cool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		var part = this._findClickedPart(e.target.parentNode);
 		if (part !== null) {
 			var partId = parseInt(part) - 1; // The first part is just a drop-site for reordering.

browser/src/control/Control.Tabs.js
@@ -3,7 +3,7 @@
  * L.Control.Tabs is used to switch sheets in Calc
  */
 
-/* global $ _ _UNO Hammer */
+/* global $ _ _UNO Hammer cool */
 L.Control.Tabs = L.Control.extend({
 	onAdd: function(map) {
 		map.on('updatepermission', this._onUpdatePermission, this);
@@ -271,6 +271,11 @@ L.Control.Tabs = L.Control.extend({
 	},
 
 	_setPart: function (e) {
+		if (cool.Comment.isAnyEdit()) {
+			cool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		var part =  e.target.id.match(/\d+/g)[0];
 		if (part !== null) {
 			this._map._docLayer._clearReferences();

browser/src/control/Parts.js
@@ -3,10 +3,15 @@
  * Document parts switching and selecting handler
  */
 
-/* global app _ */
+/* global app _ cool */
 
 L.Map.include({
 	setPart: function (part, external, calledFromSetPartHandler) {
+		if (cool.Comment.isAnyEdit()) {
+			cool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		var docLayer = this._docLayer;
 
 		if (docLayer.isCalc())
@@ -278,6 +283,11 @@ L.Map.include({
 	},
 
 	insertPage: function(nPos) {
+		if (cool.Comment.isAnyEdit()) {
+			cool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		if (this.isPresentationOrDrawing()) {
 			app.socket.sendMessage('uno .uno:InsertPage');
 		}

browser/src/layer/tile/CommentListSection.ts
@@ -5,8 +5,7 @@
 L.Map.include({
 	insertComment: function() {
 		if (cool.Comment.isAnyEdit()) {
-			this.uiManager.showInfoModal('annotation-editing', _('A comment is being edited'),
-			_('Please save or discard the comment currently being edited.'), null, _('Close'));
+			cool.CommentSection.showCommentEditingWarning();
 			return;
 		}
 		var avatar = undefined;
@@ -109,6 +108,11 @@ export class CommentSection extends CanvasSectionObject {
 			this.showSection = false;
 			this.size[0] = 0;
 		}
+	}
+
+	public static showCommentEditingWarning (): void {
+		L.Map.THIS.uiManager.showInfoModal('annotation-editing', _('A comment is being edited'),
+		_('Please save or discard the comment currently being edited.'), null, _('Close'));
 	}
 
 	private checkCollapseState(): void {

OSSII/OXOOL

browser/src/control/Control.PartsPreview.js
@@ -3,7 +3,7 @@
  * L.Control.PartsPreview
  */
 
-/* global _ app $ Hammer w2ui _UNO */
+/* global _ app $ Hammer w2ui _UNO cool */
 L.Control.PartsPreview = L.Control.extend({
 	options: {
 		fetchThumbnail: true,
@@ -499,6 +499,11 @@ L.Control.PartsPreview = L.Control.extend({
 	},
 
 	_setPart: function (e) {
+		if (oxool.Comment.isAnyEdit()) {
+			oxool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		var part = this._findClickedPart(e.target.parentNode);
 		if (part !== null) {
 			var partId = parseInt(part) - 1; // The first part is just a drop-site for reordering.

browser/src/control/Control.Tabs.js
@@ -3,7 +3,7 @@
  * L.Control.Tabs is used to switch sheets in Calc
  */
 
-/* global $ _ _UNO Hammer */
+/* global $ _ _UNO Hammer cool */
 L.Control.Tabs = L.Control.extend({
 	onAdd: function(map) {
 		map.on('updatepermission', this._onUpdatePermission, this);
@@ -271,6 +271,11 @@ L.Control.Tabs = L.Control.extend({
 	},
 
 	_setPart: function (e) {
+		if (oxool.Comment.isAnyEdit()) {
+			oxool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		var part =  e.target.id.match(/\d+/g)[0];
 		if (part !== null) {
 			this._map._docLayer._clearReferences();

browser/src/control/Parts.js
@@ -3,10 +3,15 @@
  * Document parts switching and selecting handler
  */
 
-/* global app _ */
+/* global app _ cool */
 
 L.Map.include({
 	setPart: function (part, external, calledFromSetPartHandler) {
+		if (oxool.Comment.isAnyEdit()) {
+			oxool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		var docLayer = this._docLayer;
 
 		if (docLayer.isCalc())
@@ -278,6 +283,11 @@ L.Map.include({
 	},
 
 	insertPage: function(nPos) {
+		if (oxool.Comment.isAnyEdit()) {
+			oxool.CommentSection.showCommentEditingWarning();
+			return;
+		}
+
 		if (this.isPresentationOrDrawing()) {
 			app.socket.sendMessage('uno .uno:InsertPage');
 		}

browser/src/layer/tile/CommentListSection.ts
@@ -5,8 +5,7 @@
 L.Map.include({
 	insertComment: function() {
 		if (oxool.Comment.isAnyEdit()) {
-			this.uiManager.showInfoModal('annotation-editing', _('A comment is being edited'),
-			_('Please save or discard the comment currently being edited.'), null, _('Close'));
+			oxool.CommentSection.showCommentEditingWarning();
 			return;
 		}
 		var avatar = undefined;
@@ -109,6 +108,11 @@ export class CommentSection extends CanvasSectionObject {
 			this.showSection = false;
 			this.size[0] = 0;
 		}
+	}
+
+	public static showCommentEditingWarning (): void {
+		L.Map.THIS.uiManager.showInfoModal('annotation-editing', _('A comment is being edited'),
+		_('Please save or discard the comment currently being edited.'), null, _('Close'));
 	}
 
 	private checkCollapseState(): void {