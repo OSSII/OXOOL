CollaboraOnline/online

annotation: only allow to edit one commit at a time
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Iaa021be2327294e5e34bf6b56de35894f36f263c

browser/src/layer/tile/CommentListSection.ts
@@ -4,6 +4,11 @@
 
 L.Map.include({
 	insertComment: function() {
+		if (cool.Comment.isAnyEdit()) {
+			this.uiManager.showInfoModal('annotation-editing', _('A comment is being edited'),
+			_('Please save or discard the comment currently being edited.'), null, _('Close'));
+			return;
+		}
 		var avatar = undefined;
 		var author = this.getViewName(this._docLayer._viewId);
 		if (author in this._viewInfoByUserName) {

OSSII/OXOOL

browser/src/layer/tile/CommentListSection.ts
@@ -4,6 +4,11 @@
 
 L.Map.include({
 	insertComment: function() {
+		if (oxool.Comment.isAnyEdit()) {
+			this.uiManager.showInfoModal('annotation-editing', _('A comment is being edited'),
+			_('Please save or discard the comment currently being edited.'), null, _('Close'));
+			return;
+		}
 		var avatar = undefined;
 		var author = this.getViewName(this._docLayer._viewId);
 		if (author in this._viewInfoByUserName) {