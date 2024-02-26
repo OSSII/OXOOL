CollaboraOnline/online

traceevent: re-factor and cleanup trace event emission.
Share a single emission function. Events are re-parsed and
emitted and adjusted in ClientSession; JSON escape event names.

Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
Change-Id: If489d516bf20d5ec34cfe61f7e4fb09cf2e14bf2

wsd/ClientSession.cpp
@@ -418,9 +418,9 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                     uint64_t dur;
                     if (ph == "i")
                     {
-                        COOLWSD::writeTraceEventRecording("{\"name\":\""
+                        COOLWSD::writeTraceEventRecording("{\"name\":"
                                                           + name
-                                                          + "\",\"ph\":\"i\""
+                                                          + ",\"ph\":\"i\""
                                                           + args
                                                           + ",\"ts\":"
                                                           + std::to_string(ts + _performanceCounterEpoch)
@@ -434,9 +434,9 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                              (static_cast<void>(getTokenUInt64(tokens[4], "id", id)),
                              getTokenUInt64(tokens[5], "tid", tid)))
                     {
-                        COOLWSD::writeTraceEventRecording("{\"name\":\""
+                        COOLWSD::writeTraceEventRecording("{\"name\":"
                                                           + name
-                                                          + "\",\"ph\":\""
+                                                          + ",\"ph\":\""
                                                           + ph
                                                           + "\""
                                                           + args
@@ -453,9 +453,9 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                     else if (ph == "X" &&
                              getTokenUInt64(tokens[4], "dur", dur))
                     {
-                        COOLWSD::writeTraceEventRecording("{\"name\":\""
+                        COOLWSD::writeTraceEventRecording("{\"name\":"
                                                           + name
-                                                          + "\",\"ph\":\"X\""
+                                                          + ",\"ph\":\"X\""
                                                           + args
                                                           + ",\"ts\":"
                                                           + std::to_string(ts + _performanceCounterEpoch)

OSSII/OXOOL

wsd/ClientSession.cpp
@@ -418,9 +418,9 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                     uint64_t dur;
                     if (ph == "i")
                     {
-                        OXOOLWSD::writeTraceEventRecording("{\"name\":\""
+                        OXOOLWSD::writeTraceEventRecording("{\"name\":"
                                                           + name
-                                                          + "\",\"ph\":\"i\""
+                                                          + ",\"ph\":\"i\""
                                                           + args
                                                           + ",\"ts\":"
                                                           + std::to_string(ts + _performanceCounterEpoch)
@@ -434,9 +434,9 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                              (static_cast<void>(getTokenUInt64(tokens[4], "id", id)),
                              getTokenUInt64(tokens[5], "tid", tid)))
                     {
-                        OXOOLWSD::writeTraceEventRecording("{\"name\":\""
+                        OXOOLWSD::writeTraceEventRecording("{\"name\":"
                                                           + name
-                                                          + "\",\"ph\":\""
+                                                          + ",\"ph\":\""
                                                           + ph
                                                           + "\""
                                                           + args
@@ -453,9 +453,9 @@ bool ClientSession::_handleInput(const char *buffer, int length)
                     else if (ph == "X" &&
                              getTokenUInt64(tokens[4], "dur", dur))
                     {
-                        OXOOLWSD::writeTraceEventRecording("{\"name\":\""
+                        OXOOLWSD::writeTraceEventRecording("{\"name\":"
                                                           + name
-                                                          + "\",\"ph\":\"X\""
+                                                          + ",\"ph\":\"X\""
                                                           + args
                                                           + ",\"ts\":"
                                                           + std::to_string(ts + _performanceCounterEpoch)