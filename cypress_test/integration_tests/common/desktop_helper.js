CollaboraOnline/online

cypress: introduce writer comment autosave tests
Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
Change-Id: Ic79e9cf935f29ac407a8c5ff3900f2ad1f7449a6

cypress_test/integration_tests/common/desktop_helper.js
@@ -248,7 +248,40 @@ function assertImageSize(expectedWidth, expectedHeight) {
 		});
 }
 
-function insertMultipleComment(docType, numberOfComments = 1, isMobile = false, selector) {
+function createComment(docType, text, isMobile, selector) {
+	if (docType === 'draw') {
+		cy.cGet('#menu-insert').click();
+		cy.cGet('#menu-insertcomment').click();
+	}
+	else if (!selector) {
+		actionOnSelector('insertAnnotation', (selector) => {
+			cy.cGet(selector).click();
+		});
+	}
+	else
+		cy.cGet(selector).click();
+
+	cy.wait(100);
+
+	cy.cGet('.cool-annotation-table').should('exist');
+
+	if (isMobile) {
+		cy.cGet('#input-modal-input').type(text);
+	} else {
+		cy.cGet('#annotation-modify-textarea-new').type(text);
+		cy.wait(500);
+	}
+}
+
+function saveComment(isMobile) {
+	if (isMobile) {
+		cy.cGet('#response-ok').click();
+	} else {
+		cy.cGet('#annotation-save-new').click();
+	}
+}
+
+function setupUIforCommentInsert(docType) {
 	var mode = Cypress.env('USER_INTERFACE');
 
 	if (docType === 'calc') {
@@ -280,32 +313,14 @@ function insertMultipleComment(docType, numberOfComments = 1, isMobile = false, 
 			}
 		});
 	}
+}
+
+function insertMultipleComment(docType, numberOfComments = 1, isMobile = false, selector) {
+	setupUIforCommentInsert(docType);
 
 	for (var n = 0; n < numberOfComments; n++) {
-		if (docType === 'draw') {
-			cy.cGet('#menu-insert').click();
-			cy.cGet('#menu-insertcomment').click();
-		}
-		else if (!selector) {
-			actionOnSelector('insertAnnotation', (selector) => {
-				cy.cGet(selector).click();
-			});
-		}
-		else
-			cy.cGet(selector).click();
-
-		cy.wait(100);
-
-		cy.cGet('.cool-annotation-table').should('exist');
-
-		if (isMobile) {
-			cy.cGet('#input-modal-input').type('some text' + n);
-			cy.cGet('#response-ok').click();
-		} else {
-			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
-			cy.wait(500);
-			cy.cGet('#annotation-save-new').click();
-		}
+		createComment(docType, 'some text' + n, isMobile, selector);
+		saveComment(isMobile);
 	}
 }
 
@@ -478,3 +493,6 @@ module.exports.switchUIToNotebookbar = switchUIToNotebookbar;
 module.exports.switchUIToCompact = switchUIToCompact;
 module.exports.checkAccessibilityEnabledToBe = checkAccessibilityEnabledToBe;
 module.exports.setAccessibilityState = setAccessibilityState;
+module.exports.setupUIforCommentInsert = setupUIforCommentInsert;
+module.exports.createComment = createComment;
+module.exports.saveComment = saveComment;

cypress_test/integration_tests/desktop/writer/annotation_spec.js
@@ -1,7 +1,7 @@
 /* global describe it cy require afterEach beforeEach */
 
 var helper = require('../../common/helper');
-var { insertMultipleComment, selectZoomLevel } = require('../../common/desktop_helper');
+var { insertMultipleComment, selectZoomLevel, setupUIforCommentInsert, createComment } = require('../../common/desktop_helper');
 var desktopHelper = require('../../common/desktop_helper');
 
 describe(['tagdesktop'], 'Annotation Tests', function() {
@@ -120,6 +120,197 @@ describe(['tagdesktop'], 'Collapsed Annotation Tests', function() {
 		cy.cGet('.cool-annotation-menu').click();
 		cy.cGet('body').contains('.context-menu-item','Remove').click();
 		cy.cGet('.cool-annotation-content-wrapper').should('not.exist');
+	});
+
+});
+
+describe(['tagdesktop'], 'Annotation Autosave Tests', function() {
+	var origTestFileName = 'annotation.odt';
+	var testFileName;
+
+	beforeEach(function() {
+		cy.viewport(1400, 600);
+		testFileName = helper.beforeAll(origTestFileName, 'writer');
+		desktopHelper.switchUIToNotebookbar();
+		cy.cGet('#optionscontainer div[id$="SidebarDeck.PropertyDeck"]').click(); // Hide sidebar.
+		selectZoomLevel('50');
+
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Insert autosave', function() {
+		setupUIforCommentInsert('writer');
+		createComment('writer', 'Test Comment', false, '[id=insert-insert-annotation]');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','Test Comment');
+	});
+
+	it('Insert autosave save', function() {
+		setupUIforCommentInsert('writer');
+		createComment('writer', 'Test Comment', false, '[id=insert-insert-annotation]');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-save-new').click();
+		cy.cGet('#annotation-content-area-1').should('have.text','Test Comment');
+		cy.cGet('.cool-annotation-autosavelabel').should('be.not.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','Test Comment');
+	});
+
+	it('Insert autosave cancel', function() {
+		setupUIforCommentInsert('writer');
+		createComment('writer', 'Test Comment', false, '[id=insert-insert-annotation]');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-cancel-new').click();
+		cy.cGet('#comment-container-1').should('not.exist');
+		cy.cGet('.cool-annotation-autosavelabel').should('not.exist');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('not.exist');
+		cy.cGet('#comment-container-1').should('not.exist');
+	});
+
+	it('Modify autosave', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Modify').click();
+		cy.cGet('#annotation-modify-textarea-1').type('some other text, ');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some other text, some text0');
+	});
+
+	it('Modify autosave save', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Modify').click();
+		cy.cGet('#annotation-modify-textarea-1').type('some other text, ');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-save-1').click();
+		cy.cGet('#annotation-content-area-1').should('have.text','some other text, some text0');
+		cy.cGet('.cool-annotation-autosavelabel').should('be.not.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some other text, some text0');
+	});
+
+	it.skip('Modify autosave cancel', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Modify').click();
+		cy.cGet('#annotation-modify-textarea-1').type('some other text, ');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-cancel-1').click();
+		cy.cGet('.cool-annotation-edit.modify-annotation').should('be.not.visible');
+		cy.cGet('.cool-annotation-autosavelabel').should('be.not.visible');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+	});
+
+	it('Reply autosave', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Reply').click();
+		cy.cGet('#annotation-reply-textarea-1').type('some reply text');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.reply-annotation').should('be.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-2').should('have.text','some reply text');
+	});
+
+	it.skip('Reply autosave save', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Reply').click();
+		cy.cGet('#annotation-reply-textarea-1').type('some reply text');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.reply-annotation').should('be.visible');
+		cy.cGet('#annotation-reply-1').click();
+		cy.cGet('.cool-annotation-edit.reply-annotation').should('be.not.visible');
+		cy.cGet('.cool-annotation-autosavelabel').should('be.not.visible');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#annotation-content-area-2').should('have.text','some reply text');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-2').should('have.text','some reply text');
+	});
+
+	it.skip('Reply autosave cancel', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Reply').click();
+		cy.cGet('#annotation-reply-textarea-1').type('some reply text');
+		cy.cGet('#map').focus();
+		cy.cGet('.cool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.cool-annotation-edit.reply-annotation').should('be.visible');
+		cy.cGet('#annotation-cancel-reply-1').click();
+		cy.cGet('.cool-annotation-edit.reply-annotation').should('be.not.visible');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#annotation-content-area-2').should('not.exist');
+		cy.cGet('#comment-container-1 .cool-annotation-autosavelabel').should('be.not.visible');
+		cy.cGet('#comment-container-2 .cool-annotation-autosavelabel').should('not.exist');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.cool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#annotation-content-area-2').should('not.exist');
 	});
 
 });

OSSII/OXOOL

cypress_test/integration_tests/common/desktop_helper.js
@@ -248,7 +248,40 @@ function assertImageSize(expectedWidth, expectedHeight) {
 		});
 }
 
-function insertMultipleComment(docType, numberOfComments = 1, isMobile = false, selector) {
+function createComment(docType, text, isMobile, selector) {
+	if (docType === 'draw') {
+		cy.cGet('#menu-insert').click();
+		cy.cGet('#menu-insertcomment').click();
+	}
+	else if (!selector) {
+		actionOnSelector('insertAnnotation', (selector) => {
+			cy.cGet(selector).click();
+		});
+	}
+	else
+		cy.cGet(selector).click();
+
+	cy.wait(100);
+
+	cy.cGet('.oxool-annotation-table').should('exist');
+
+	if (isMobile) {
+		cy.cGet('#input-modal-input').type(text);
+	} else {
+		cy.cGet('#annotation-modify-textarea-new').type(text);
+		cy.wait(500);
+	}
+}
+
+function saveComment(isMobile) {
+	if (isMobile) {
+		cy.cGet('#response-ok').click();
+	} else {
+		cy.cGet('#annotation-save-new').click();
+	}
+}
+
+function setupUIforCommentInsert(docType) {
 	var mode = Cypress.env('USER_INTERFACE');
 
 	if (docType === 'calc') {
@@ -280,32 +313,14 @@ function insertMultipleComment(docType, numberOfComments = 1, isMobile = false, 
 			}
 		});
 	}
+}
+
+function insertMultipleComment(docType, numberOfComments = 1, isMobile = false, selector) {
+	setupUIforCommentInsert(docType);
 
 	for (var n = 0; n < numberOfComments; n++) {
-		if (docType === 'draw') {
-			cy.cGet('#menu-insert').click();
-			cy.cGet('#menu-insertcomment').click();
-		}
-		else if (!selector) {
-			actionOnSelector('insertAnnotation', (selector) => {
-				cy.cGet(selector).click();
-			});
-		}
-		else
-			cy.cGet(selector).click();
-
-		cy.wait(100);
-
-		cy.cGet('.oxool-annotation-table').should('exist');
-
-		if (isMobile) {
-			cy.cGet('#input-modal-input').type('some text' + n);
-			cy.cGet('#response-ok').click();
-		} else {
-			cy.cGet('#annotation-modify-textarea-new').type('some text' + n);
-			cy.wait(500);
-			cy.cGet('#annotation-save-new').click();
-		}
+		createComment(docType, 'some text' + n, isMobile, selector);
+		saveComment(isMobile);
 	}
 }
 
@@ -478,3 +493,6 @@ module.exports.switchUIToNotebookbar = switchUIToNotebookbar;
 module.exports.switchUIToCompact = switchUIToCompact;
 module.exports.checkAccessibilityEnabledToBe = checkAccessibilityEnabledToBe;
 module.exports.setAccessibilityState = setAccessibilityState;
+module.exports.setupUIforCommentInsert = setupUIforCommentInsert;
+module.exports.createComment = createComment;
+module.exports.saveComment = saveComment;

cypress_test/integration_tests/desktop/writer/annotation_spec.js
@@ -1,7 +1,7 @@
 /* global describe it cy require afterEach beforeEach */
 
 var helper = require('../../common/helper');
-var { insertMultipleComment, selectZoomLevel } = require('../../common/desktop_helper');
+var { insertMultipleComment, selectZoomLevel, setupUIforCommentInsert, createComment } = require('../../common/desktop_helper');
 var desktopHelper = require('../../common/desktop_helper');
 
 describe(['tagdesktop'], 'Annotation Tests', function() {
@@ -120,6 +120,197 @@ describe(['tagdesktop'], 'Collapsed Annotation Tests', function() {
 		cy.cGet('.oxool-annotation-menu').click();
 		cy.cGet('body').contains('.context-menu-item','Remove').click();
 		cy.cGet('.oxool-annotation-content-wrapper').should('not.exist');
+	});
+
+});
+
+describe(['tagdesktop'], 'Annotation Autosave Tests', function() {
+	var origTestFileName = 'annotation.odt';
+	var testFileName;
+
+	beforeEach(function() {
+		cy.viewport(1400, 600);
+		testFileName = helper.beforeAll(origTestFileName, 'writer');
+		desktopHelper.switchUIToNotebookbar();
+		cy.cGet('#optionscontainer div[id$="SidebarDeck.PropertyDeck"]').click(); // Hide sidebar.
+		selectZoomLevel('50');
+
+	});
+
+	afterEach(function() {
+		helper.afterAll(testFileName, this.currentTest.state);
+	});
+
+	it('Insert autosave', function() {
+		setupUIforCommentInsert('writer');
+		createComment('writer', 'Test Comment', false, '[id=insert-insert-annotation]');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','Test Comment');
+	});
+
+	it('Insert autosave save', function() {
+		setupUIforCommentInsert('writer');
+		createComment('writer', 'Test Comment', false, '[id=insert-insert-annotation]');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-save-new').click();
+		cy.cGet('#annotation-content-area-1').should('have.text','Test Comment');
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.not.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','Test Comment');
+	});
+
+	it('Insert autosave cancel', function() {
+		setupUIforCommentInsert('writer');
+		createComment('writer', 'Test Comment', false, '[id=insert-insert-annotation]');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-cancel-new').click();
+		cy.cGet('#comment-container-1').should('not.exist');
+		cy.cGet('.oxool-annotation-autosavelabel').should('not.exist');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('not.exist');
+		cy.cGet('#comment-container-1').should('not.exist');
+	});
+
+	it('Modify autosave', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Modify').click();
+		cy.cGet('#annotation-modify-textarea-1').type('some other text, ');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some other text, some text0');
+	});
+
+	it('Modify autosave save', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Modify').click();
+		cy.cGet('#annotation-modify-textarea-1').type('some other text, ');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-save-1').click();
+		cy.cGet('#annotation-content-area-1').should('have.text','some other text, some text0');
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.not.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some other text, some text0');
+	});
+
+	it.skip('Modify autosave cancel', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Modify').click();
+		cy.cGet('#annotation-modify-textarea-1').type('some other text, ');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.visible');
+		cy.cGet('#annotation-cancel-1').click();
+		cy.cGet('.oxool-annotation-edit.modify-annotation').should('be.not.visible');
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.not.visible');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+	});
+
+	it('Reply autosave', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Reply').click();
+		cy.cGet('#annotation-reply-textarea-1').type('some reply text');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.reply-annotation').should('be.visible');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-2').should('have.text','some reply text');
+	});
+
+	it.skip('Reply autosave save', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Reply').click();
+		cy.cGet('#annotation-reply-textarea-1').type('some reply text');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.reply-annotation').should('be.visible');
+		cy.cGet('#annotation-reply-1').click();
+		cy.cGet('.oxool-annotation-edit.reply-annotation').should('be.not.visible');
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.not.visible');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#annotation-content-area-2').should('have.text','some reply text');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-2').should('have.text','some reply text');
+	});
+
+	it.skip('Reply autosave cancel', function() {
+		insertMultipleComment('writer', 1, false, '[id=insert-insert-annotation]');
+
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#comment-annotation-menu-1').click();
+		cy.cGet('body').contains('.context-menu-item', 'Reply').click();
+		cy.cGet('#annotation-reply-textarea-1').type('some reply text');
+		cy.cGet('#map').focus();
+		cy.cGet('.oxool-annotation-autosavelabel').should('be.visible');
+		cy.cGet('.oxool-annotation-edit.reply-annotation').should('be.visible');
+		cy.cGet('#annotation-cancel-reply-1').click();
+		cy.cGet('.oxool-annotation-edit.reply-annotation').should('be.not.visible');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#annotation-content-area-2').should('not.exist');
+		cy.cGet('#comment-container-1 .oxool-annotation-autosavelabel').should('be.not.visible');
+		cy.cGet('#comment-container-2 .oxool-annotation-autosavelabel').should('not.exist');
+
+		helper.closeDocument(testFileName, '');
+		helper.beforeAll(testFileName, 'writer', true, false, false, true);
+		cy.cGet('.oxool-annotation-content-wrapper').should('exist');
+		cy.cGet('#annotation-content-area-1').should('have.text','some text0');
+		cy.cGet('#annotation-content-area-2').should('not.exist');
 	});
 
 });