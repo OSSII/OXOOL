CollaboraOnline/online

cool#8023 Remove HTML marker in unit-copy-paste
This really just passed by accident in the past:

	Clipboard with 19 entries:
		[0] - size 5689 type: 'application/x-openoffice-embed-source-xml;windows_formatname="Star Embed Source (XML)"'
		[1] - size 159 type: 'application/x-openoffice-objectdescriptor-xml;windows_formatname="Star Object Descriptor (XML)";classname="47BBB4CB-CE4C-4E80-a591-42d9ae74950f";typename="Collabora OfficeDev 23.05 Spreadsheet";displayname="file:///tmp/user/docs/jexG9292sB0PuaWL/UnitCopyPaste1d0fdba5_empty.ods";viewaspect="1";width="6783";height="2712";posx="0";posy="0"'
		[2] - size 388 type: 'application/x-openoffice-gdimetafile;windows_formatname="GDIMetaFile"'
		[3] - size 1812 type: 'application/x-openoffice-emf;windows_formatname="Image EMF"'
		[4] - size 2222 type: 'application/x-openoffice-wmf;windows_formatname="Image WMF"'
		[5] - size 1073 type: 'image/png'
		[6] - size 79158 type: 'application/x-openoffice-bitmap;windows_formatname="Bitmap"'
		[7] - size 79158 type: 'image/bmp'
		[8] - size 1493 type: 'text/html'
		[9] - size 0 type: '    </tr>'
		[10] - size 0 type: '           <td align="left"><br></td>'
		[11] - size 0 type: '</table>'
		[12] - size 49 type: 'application/x-openoffice-sylk;windows_formatname="Sylk"'
		[13] - size 98 type: 'application/x-openoffice-link;windows_formatname="Link"'
		[14] - size 246 type: 'application/x-openoffice-dif;windows_formatname="DIF"'
		[15] - size 6 type: 'text/plain;charset=utf-8'
		[16] - size 6 type: 'application/x-libreoffice-tsvc'
		[17] - size 977 type: 'text/rtf'
		[18] - size 977 type: 'text/richtext'

Remove the marker, then sizes will match again.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I1edea3d3a64b9a54cc1c0a0cea703ae66c2aba82

test/UnitCopyPaste.cpp
@@ -93,7 +93,9 @@ public:
                                      std::string("application/octet-stream"),
                                      httpResponse->header().getContentType());
 
-            std::istringstream responseStream(httpResponse->getBody());
+            std::string body = httpResponse->getBody();
+            removeSessionClipboardMeta(body);
+            std::istringstream responseStream(body);
             auto clipboard = std::make_shared<ClipboardData>();
             clipboard->read(responseStream);
             std::ostringstream oss;
@@ -206,21 +208,39 @@ public:
         return true;
     }
 
+    std::shared_ptr<ClientSession> getChildSession(size_t session)
+    {
+        std::shared_ptr<DocumentBroker> broker;
+        std::shared_ptr<ClientSession> clientSession;
+
+        std::vector<std::shared_ptr<DocumentBroker>> brokers = COOLWSD::getBrokersTestOnly();
+        assert(brokers.size() > 0);
+        broker = brokers[0];
+        auto sessions = broker->getSessionsTestOnlyUnsafe();
+        assert(sessions.size() > 0 && session < sessions.size());
+        return sessions[session];
+    }
+
     std::string getSessionClipboardURI(size_t session)
     {
-            std::shared_ptr<DocumentBroker> broker;
-            std::shared_ptr<ClientSession> clientSession;
-
-            std::vector<std::shared_ptr<DocumentBroker>> brokers = COOLWSD::getBrokersTestOnly();
-            assert(brokers.size() > 0);
-            broker = brokers[0];
-            auto sessions = broker->getSessionsTestOnlyUnsafe();
-            assert(sessions.size() > 0 && session < sessions.size());
-            clientSession = sessions[session];
+            std::shared_ptr<ClientSession> clientSession = getChildSession(session);
 
             std::string tag = clientSession->getClipboardURI(false); // nominally thread unsafe
             LOG_TST("Got tag '" << tag << "' for session " << session);
             return tag;
+    }
+
+    void removeSessionClipboardMeta(std::string& payload)
+    {
+        if (COOLWSD::getBrokersTestOnly().empty())
+        {
+            return;
+        }
+
+        // The removal doesn't depend on the clipboard URL, so just ask the first session to do the
+        // removal for us.
+        std::shared_ptr<ClientSession> clientSession = getChildSession(0);
+        clientSession->preProcessSetClipboardPayload(payload);
     }
 
     std::string buildClipboardText(const std::string &text)

OSSII/OXOOL

(cherry picked from commit 141e1ce7d08f6b093a4cefbce2a7502ca32fc21b)

test/UnitCopyPaste.cpp
@@ -93,7 +93,9 @@ public:
                                      std::string("application/octet-stream"),
                                      httpResponse->header().getContentType());
 
-            std::istringstream responseStream(httpResponse->getBody());
+            std::string body = httpResponse->getBody();
+            removeSessionClipboardMeta(body);
+            std::istringstream responseStream(body);
             auto clipboard = std::make_shared<ClipboardData>();
             clipboard->read(responseStream);
             std::ostringstream oss;
@@ -206,21 +208,39 @@ public:
         return true;
     }
 
+    std::shared_ptr<ClientSession> getChildSession(size_t session)
+    {
+        std::shared_ptr<DocumentBroker> broker;
+        std::shared_ptr<ClientSession> clientSession;
+
+        std::vector<std::shared_ptr<DocumentBroker>> brokers = OXOOLWSD::getBrokersTestOnly();
+        assert(brokers.size() > 0);
+        broker = brokers[0];
+        auto sessions = broker->getSessionsTestOnlyUnsafe();
+        assert(sessions.size() > 0 && session < sessions.size());
+        return sessions[session];
+    }
+
     std::string getSessionClipboardURI(size_t session)
     {
-            std::shared_ptr<DocumentBroker> broker;
-            std::shared_ptr<ClientSession> clientSession;
-
-            std::vector<std::shared_ptr<DocumentBroker>> brokers = OXOOLWSD::getBrokersTestOnly();
-            assert(brokers.size() > 0);
-            broker = brokers[0];
-            auto sessions = broker->getSessionsTestOnlyUnsafe();
-            assert(sessions.size() > 0 && session < sessions.size());
-            clientSession = sessions[session];
+            std::shared_ptr<ClientSession> clientSession = getChildSession(session);
 
             std::string tag = clientSession->getClipboardURI(false); // nominally thread unsafe
             LOG_TST("Got tag '" << tag << "' for session " << session);
             return tag;
+    }
+
+    void removeSessionClipboardMeta(std::string& payload)
+    {
+        if (OXOOLWSD::getBrokersTestOnly().empty())
+        {
+            return;
+        }
+
+        // The removal doesn't depend on the clipboard URL, so just ask the first session to do the
+        // removal for us.
+        std::shared_ptr<ClientSession> clientSession = getChildSession(0);
+        clientSession->preProcessSetClipboardPayload(payload);
     }
 
     std::string buildClipboardText(const std::string &text)