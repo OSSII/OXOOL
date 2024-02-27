CollaboraOnline/online

browser: add logging token to send to server side
Change-Id: I252665637a3c5079e0c433f0b1a6c8197105e45f
Signed-off-by: Henry Castro <hcastro@collabora.com>

browser/js/global.js
@@ -24,7 +24,7 @@ window.app = {
 			fetch(global.location.pathname.match(/.*\//) + 'logging.html', {
 				method: 'POST',
 				headers: { 'Content-Type' : 'application/json' },
-				body: log
+				body: global.coolLogging + ' ' + log
 			});
 		}
 	};
@@ -78,7 +78,7 @@ window.app = {
 		}
 	};
 
-	global.setLogging(global.coolLogging == 'true');
+	global.setLogging(global.coolLogging != '');
 
 	global.coolParams = new URLSearchParams(global.location.search);
 	var ua = navigator.userAgent.toLowerCase(),

OSSII/OXOOL

browser/js/global.js
@@ -24,7 +24,7 @@ window.app = {
 			fetch(global.location.pathname.match(/.*\//) + 'logging.html', {
 				method: 'POST',
 				headers: { 'Content-Type' : 'application/json' },
-				body: log
+				body: global.oxoolLogging + ' ' + log
 			});
 		}
 	};
@@ -78,7 +78,7 @@ window.app = {
 		}
 	};
 
-	global.setLogging(global.oxoolLogging == 'true');
+	global.setLogging(global.oxoolLogging != '');
 
 	global.oxoolParams = new URLSearchParams(global.location.search);
 	var ua = navigator.userAgent.toLowerCase(),