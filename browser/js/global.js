CollaboraOnline/online

lang ended up as "(null)" and docs from debug.html didn't load
Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Change-Id: Ie007dbdc2d4066c657d2a1cd05c6f4532404495b

browser/js/global.js
@@ -1113,9 +1113,9 @@ window.app = {
 		}
 	}
 
-	var lang = encodeURIComponent(global.coolParams.get('lang'));
+	var lang = global.coolParams.get('lang');
 	if (lang)
-		global.langParam = lang;
+		global.langParam = encodeURIComponent(lang);
 	else
 		global.langParam = 'en-US';
 	global.langParamLocale = new Intl.Locale(global.langParam);

OSSII/OXOOL

browser/js/global.js
@@ -1113,9 +1113,9 @@ window.app = {
 		}
 	}
 
-	var lang = encodeURIComponent(global.oxoolParams.get('lang'));
+	var lang = global.oxoolParams.get('lang');
 	if (lang)
-		global.langParam = lang;
+		global.langParam = encodeURIComponent(lang);
 	else
 		global.langParam = 'en-US';
 	global.langParamLocale = new Intl.Locale(global.langParam);