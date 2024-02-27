CollaboraOnline/online

cool#8023 wsd, ClientSession: add new style clipboard marker to our HTML output
The HTML we produce on copy has to go via a sanitize process on paste
once we start using the new clipboard API at
<https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/read>.

The trouble is that at least Chrome simply strips away all <meta> HTML
tags, we can no longer recognize our own HTML output, preventing us from
doing a better internal paste.

Fix this by switching to a markup with survives the cleanup: a <div>
around the <html>...</html> fragment.

This just introduces the new markup, doesn't replace the old markup yet.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I75b00d00064ce452a57185c62f4d99a6d052d867

wsd/ClientSession.cpp
@@ -1624,6 +1624,33 @@ void ClientSession::postProcessCopyPayload(const std::shared_ptr<Message>& paylo
                 return false;
             }
         });
+
+    // New-style: <div> around <html>, that is not sanitized by Chrome.
+    payload->rewriteDataBody([=](std::vector<char>& data) {
+            std::size_t pos = Util::findInVector(data, "<html>");
+
+            if (pos != std::string::npos)
+            {
+                const std::string meta = getClipboardURI();
+                LOG_TRC("Inject clipboard cool origin of '" << meta << "'");
+                std::string origin = "<div id=\"meta-origin\" data-coolorigin=\"" + meta + "\">\n";
+                data.insert(data.begin() + pos, origin.begin(), origin.end());
+
+                const char* end = "</html>";
+                pos = Util::findInVector(data, end);
+                if (pos != std::string::npos)
+                {
+                    origin = "</div>";
+                    data.insert(data.begin() + pos + strlen(end), origin.begin(), origin.end());
+                }
+                return true;
+            }
+            else
+            {
+                LOG_DBG("Missing <html> in textselectioncontent/clipboardcontent payload.");
+                return false;
+            }
+        });
 }
 
 bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& payload)

OSSII/OXOOL

(cherry picked from commit 819dd9c682b43be5386097f36ddaad51d9e013dd)

wsd/ClientSession.cpp
@@ -1660,6 +1660,33 @@ void ClientSession::postProcessCopyPayload(const std::shared_ptr<Message>& paylo
                 return false;
             }
         });
+
+    // New-style: <div> around <html>, that is not sanitized by Chrome.
+    payload->rewriteDataBody([=](std::vector<char>& data) {
+            std::size_t pos = Util::findInVector(data, "<html>");
+
+            if (pos != std::string::npos)
+            {
+                const std::string meta = getClipboardURI();
+                LOG_TRC("Inject clipboard cool origin of '" << meta << "'");
+                std::string origin = "<div id=\"meta-origin\" data-oxoolorigin=\"" + meta + "\">\n";
+                data.insert(data.begin() + pos, origin.begin(), origin.end());
+
+                const char* end = "</html>";
+                pos = Util::findInVector(data, end);
+                if (pos != std::string::npos)
+                {
+                    origin = "</div>";
+                    data.insert(data.begin() + pos + strlen(end), origin.begin(), origin.end());
+                }
+                return true;
+            }
+            else
+            {
+                LOG_DBG("Missing <html> in textselectioncontent/clipboardcontent payload.");
+                return false;
+            }
+        });
 }
 
 bool ClientSession::handleKitToClientMessage(const std::shared_ptr<Message>& payload)