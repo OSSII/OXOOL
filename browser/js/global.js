CollaboraOnline/online

Add url parameter to launch with random settings
Sets random language and launches debug mode,
turning on the Randomize User Settings tool

Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
Change-Id: I5e06be624cd5383784ea640304980a82e76be229

browser/js/global.js
@@ -1118,6 +1118,21 @@ window.app = {
 		}
 	}
 
+	var isRandomUser = global.coolParams.get('randomUser');
+	if (isRandomUser) {
+		// List of languages supported in core
+		var randomUserLangs = [
+			'ar', 'bg', 'ca', 'cs', 'da', 'de', 'el', 'en-US',
+			'en-GB', 'eo', 'es', 'eu', 'fi', 'fr', 'gl', 'he',
+			'hr', 'hu', 'id', 'is', 'it', 'ja', 'ko', 'lo',
+			'nb', 'nl', 'oc', 'pl', 'pt', 'pt-BR', 'sq', 'ru',
+			'sk', 'sl', 'sv', 'tr', 'uk', 'vi', 'zh-CN', 'zh-TW'];
+		var randomUserLang = randomUserLangs[Math.floor(Math.random() * randomUserLangs.length)];
+		window.app.console.log('Randomize Settings: Set language to: ',randomUserLang);
+		global.coolParams.set('lang', randomUserLang);
+		global.coolParams.set('debug',true);
+	}
+
 	var lang = global.coolParams.get('lang');
 	if (lang)
 		global.langParam = encodeURIComponent(lang);

OSSII/OXOOL

browser/js/global.js
@@ -1113,6 +1113,21 @@ window.app = {
 		}
 	}
 
+	var isRandomUser = global.oxoolParams.get('randomUser');
+	if (isRandomUser) {
+		// List of languages supported in core
+		var randomUserLangs = [
+			'ar', 'bg', 'ca', 'cs', 'da', 'de', 'el', 'en-US',
+			'en-GB', 'eo', 'es', 'eu', 'fi', 'fr', 'gl', 'he',
+			'hr', 'hu', 'id', 'is', 'it', 'ja', 'ko', 'lo',
+			'nb', 'nl', 'oc', 'pl', 'pt', 'pt-BR', 'sq', 'ru',
+			'sk', 'sl', 'sv', 'tr', 'uk', 'vi', 'zh-CN', 'zh-TW'];
+		var randomUserLang = randomUserLangs[Math.floor(Math.random() * randomUserLangs.length)];
+		window.app.console.log('Randomize Settings: Set language to: ',randomUserLang);
+		global.oxoolParams.set('lang', randomUserLang);
+		global.oxoolParams.set('debug',true);
+	}
+
 	var lang = global.oxoolParams.get('lang');
 	if (lang)
 		global.langParam = encodeURIComponent(lang);