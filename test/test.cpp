CollaboraOnline/online

Fix for unit-httpws failure
Fix race condition in testSaveOnDisconnect by waiting for kit process to
stop instead of just counting total kit processes

Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
Change-Id: I6bda6b114070123a1366bc04eac1873f19928ac0

test/test.cpp
@@ -300,6 +300,14 @@ std::set<pid_t> getKitPids()
 {
     return COOLWSD::getKitPids();
 }
+std::set<pid_t> getSpareKitPids()
+{
+    return COOLWSD::getSpareKitPids();
+}
+std::set<pid_t> getDocKitPids()
+{
+    return COOLWSD::getDocKitPids();
+}
 
 /// Get the PID of the forkit
 std::set<pid_t> getForKitPids()

OSSII/OXOOL

test/test.cpp
@@ -300,6 +300,14 @@ std::set<pid_t> getKitPids()
 {
     return OXOOLWSD::getKitPids();
 }
+std::set<pid_t> getSpareKitPids()
+{
+    return OXOOLWSD::getSpareKitPids();
+}
+std::set<pid_t> getDocKitPids()
+{
+    return OXOOLWSD::getDocKitPids();
+}
 
 /// Get the PID of the forkit
 std::set<pid_t> getForKitPids()