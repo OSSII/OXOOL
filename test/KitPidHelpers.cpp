CollaboraOnline/online

Move kit pid helper functions to one file
Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
Change-Id: If77b8ac3c7493f4d44b510ec54b68292856f89d5

test/KitPidHelpers.cpp
@@ -8,29 +8,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+#include "KitPidHelpers.hpp"
 
-#pragma once
-
+#include <set>
 #include <chrono>
 #include <iostream>
 #include <thread>
 
-#include <cppunit/extensions/HelperMacros.h>
-
-#include <Poco/DirectoryIterator.h>
-#include <Poco/FileStream.h>
-#include <Poco/StreamCopier.h>
-
+#include <wsd/COOLWSD.hpp>
 #include <Common.hpp>
-#include "Util.hpp"
-#include "lokassert.hpp"
-#include "test.hpp"
-#include "testlog.hpp"
+#include <Util.hpp>
 
-static int countCoolKitProcesses(const int expected,
-                                 std::chrono::milliseconds timeoutMs
-                                 = std::chrono::milliseconds(COMMAND_TIMEOUT_MS * 8))
+#include <lokassert.hpp>
+#include <test.hpp>
+#include <testlog.hpp>
+
+std::string getPidList(std::set<pid_t> pids);
+
+std::set<pid_t> helpers::getKitPids() { return COOLWSD::getKitPids(); }
+
+std::set<pid_t> helpers::getSpareKitPids() { return COOLWSD::getSpareKitPids(); }
+
+std::set<pid_t> helpers::getDocKitPids() { return COOLWSD::getDocKitPids(); }
+
+/// Get the PID of the forkit
+std::set<pid_t> helpers::getForKitPids()
 {
+    std::set<pid_t> pids;
+    if (COOLWSD::ForKitProcId >= 0)
+        pids.emplace(COOLWSD::ForKitProcId);
+    return pids;
+}
+
+/// How many live coolkit processes do we have ?
+int helpers::getCoolKitProcessCount()
+{
+    return getKitPids().size();
+}
+
+int helpers::countCoolKitProcesses(const int expected)
+{
+    std::chrono::milliseconds timeoutMs = std::chrono::milliseconds(COMMAND_TIMEOUT_MS) * 8;
     const auto testname = "countCoolKitProcesses ";
     TST_LOG_BEGIN("Waiting until coolkit processes are exactly " << expected << ". Coolkits: ");
 
@@ -64,6 +82,7 @@ static int countCoolKitProcesses(const int expected,
     }
 
     TST_LOG_END;
+    LOK_ASSERT(expected == count);
     if (expected != count)
     {
         TST_LOG_BEGIN("Found " << count << " LoKit processes but was expecting " << expected << ": [");
@@ -87,37 +106,69 @@ static int countCoolKitProcesses(const int expected,
     return count;
 }
 
-// FIXME: we probably should make this extern
-// and reuse it. As it stands now, it is per
-// translation unit, which isn't desirable if
-// (in the non-ideal event that) it's not 1,
-// it will cause testNoExtraCoolKitsLeft to
-// wait unnecessarily and fail.
-static int InitialCoolKitCount = 1;
-static std::chrono::steady_clock::time_point TestStartTime;
-
-static void testCountHowManyCoolkits()
+void helpers::testCountHowManyCoolkits()
 {
     const char testname[] = "countHowManyCoolkits ";
-    TestStartTime = std::chrono::steady_clock::now();
+    resetTestStartTime();
 
-    InitialCoolKitCount = countCoolKitProcesses(InitialCoolKitCount);
+    countCoolKitProcesses(InitialCoolKitCount);
     TST_LOG("Initial coolkit count is " << InitialCoolKitCount);
     LOK_ASSERT(InitialCoolKitCount > 0);
 
-    TestStartTime = std::chrono::steady_clock::now();
+    resetTestStartTime();
 }
 
-static void testNoExtraCoolKitsLeft()
+void helpers::testNoExtraCoolKitsLeft()
 {
     const char testname[] = "noExtraCoolKitsLeft ";
     const int countNow = countCoolKitProcesses(InitialCoolKitCount);
     LOK_ASSERT_EQUAL(InitialCoolKitCount, countNow);
 
-    const auto duration = (std::chrono::steady_clock::now() - TestStartTime);
-    const auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    const auto durationMs = timeSinceTestStartMs();
 
     TST_LOG(" (" << durationMs << ')');
 }
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+void helpers::waitForKitProcessToStop(
+        const pid_t pid,
+        const std::string& testname,
+        const std::chrono::milliseconds timeoutMs /* = COMMAND_TIMEOUT_MS * 8 */,
+        const std::chrono::milliseconds retryMs /* = 10ms */)
+{
+    TST_LOG("Waiting for kit process " << pid << " to stop.");
+
+    std::set<pid_t> pids = getDocKitPids();
+    TST_LOG("Active kit pids are: " << getPidList(pids));
+
+    int tries = (timeoutMs / retryMs);
+    while(pids.contains(pid) && tries >= 0)
+    {
+        std::this_thread::sleep_for(retryMs);
+        pids = getDocKitPids();
+        tries--;
+    }
+
+    if (pids.contains(pid))
+    {
+        std::ostringstream oss;
+        oss << "Timed out waiting for kit process " << pid << " to stop. Active kit pids are: " << getPidList(pids);
+        LOK_ASSERT_FAIL(oss.str());
+    }
+    else
+    {
+        TST_LOG("Finished waiting for kit process " << pid << " to stop.");
+        TST_LOG("Active kit pids are: " << getPidList(pids));
+    }
+}
+
+std::string getPidList(std::set<pid_t> pids)
+{
+    std::ostringstream oss;
+    oss << "[";
+    for (pid_t i : pids)
+    {
+        oss << i << ", ";
+    }
+    oss << "]";
+    return oss.str();
+}

test/httpcrashtest.cpp
@@ -39,7 +39,7 @@
 #include <Protocol.hpp>
 #include <test.hpp>
 #include <helpers.hpp>
-#include <countcoolkits.hpp>
+#include <KitPidHelpers.hpp>
 
 using namespace helpers;
 
@@ -153,7 +153,7 @@ void HTTPCrashTest::testCrashKit()
         TST_LOG("Killing coolkit instances.");
 
         killLoKitProcesses();
-        countCoolKitProcesses(0, std::chrono::seconds(1));
+        countCoolKitProcesses(0);//, std::chrono::seconds(1));
 
         TST_LOG("Reading the error code from the socket.");
         //FIXME: implement in WebSocketSession.
@@ -188,7 +188,7 @@ void HTTPCrashTest::testRecoverAfterKitCrash()
         TST_LOG("Killing coolkit instances.");
 
         killLoKitProcesses();
-        countCoolKitProcesses(0, std::chrono::seconds(1));
+        countCoolKitProcesses(0);//, std::chrono::seconds(1));
 
         // We expect the client connection to close.
         TST_LOG("Reconnect after kill.");
@@ -270,7 +270,6 @@ static void killPids(const std::set<pid_t> &pids, const std::string& testname)
 void HTTPCrashTest::killLoKitProcesses()
 {
     killPids(getKitPids(), "killLoKitProcesses ");
-    InitialCoolKitCount = 1; // non-intuitive but it will arrive soon.
 }
 
 void HTTPCrashTest::killForkitProcess()

test/integration-http-server.cpp
@@ -18,7 +18,7 @@
 #include <Session.hpp>
 #include <Common.hpp>
 #include <common/FileUtil.hpp>
-#include <countcoolkits.hpp>
+#include <KitPidHelpers.hpp>
 
 #include <Poco/Net/AcceptCertificateHandler.h>
 #include <Poco/Net/FilePartSource.h>
@@ -100,14 +100,14 @@ public:
     void setUp()
     {
         helpers::resetTestStartTime();
-        testCountHowManyCoolkits();
+        helpers::testCountHowManyCoolkits();
         helpers::resetTestStartTime();
     }
 
     void tearDown()
     {
         helpers::resetTestStartTime();
-        testNoExtraCoolKitsLeft();
+        helpers::testNoExtraCoolKitsLeft();
         helpers::resetTestStartTime();
     }
 

OSSII/OXOOL

test/KitPidHelpers.cpp
@@ -8,29 +8,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+#include "KitPidHelpers.hpp"
 
-#pragma once
-
+#include <set>
 #include <chrono>
 #include <iostream>
 #include <thread>
 
-#include <cppunit/extensions/HelperMacros.h>
-
-#include <Poco/DirectoryIterator.h>
-#include <Poco/FileStream.h>
-#include <Poco/StreamCopier.h>
-
+#include <wsd/OXOOLWSD.hpp>
 #include <Common.hpp>
-#include "Util.hpp"
-#include "lokassert.hpp"
-#include "test.hpp"
-#include "testlog.hpp"
+#include <Util.hpp>
 
-static int countOxoolKitProcesses(const int expected,
-                                 std::chrono::milliseconds timeoutMs
-                                 = std::chrono::milliseconds(COMMAND_TIMEOUT_MS * 8))
+#include <lokassert.hpp>
+#include <test.hpp>
+#include <testlog.hpp>
+
+std::string getPidList(std::set<pid_t> pids);
+
+std::set<pid_t> helpers::getKitPids() { return OXOOLWSD::getKitPids(); }
+
+std::set<pid_t> helpers::getSpareKitPids() { return OXOOLWSD::getSpareKitPids(); }
+
+std::set<pid_t> helpers::getDocKitPids() { return OXOOLWSD::getDocKitPids(); }
+
+/// Get the PID of the forkit
+std::set<pid_t> helpers::getForKitPids()
 {
+    std::set<pid_t> pids;
+    if (OXOOLWSD::ForKitProcId >= 0)
+        pids.emplace(OXOOLWSD::ForKitProcId);
+    return pids;
+}
+
+/// How many live oxoolkit processes do we have ?
+int helpers::getOxoolKitProcessCount()
+{
+    return getKitPids().size();
+}
+
+int helpers::countOxoolKitProcesses(const int expected)
+{
+    std::chrono::milliseconds timeoutMs = std::chrono::milliseconds(COMMAND_TIMEOUT_MS) * 8;
     const auto testname = "countOxoolKitProcesses ";
     TST_LOG_BEGIN("Waiting until oxoolkit processes are exactly " << expected << ". Oxoolkits: ");
 
@@ -64,6 +82,7 @@ static int countOxoolKitProcesses(const int expected,
     }
 
     TST_LOG_END;
+    LOK_ASSERT(expected == count);
     if (expected != count)
     {
         TST_LOG_BEGIN("Found " << count << " LoKit processes but was expecting " << expected << ": [");
@@ -87,37 +106,69 @@ static int countOxoolKitProcesses(const int expected,
     return count;
 }
 
-// FIXME: we probably should make this extern
-// and reuse it. As it stands now, it is per
-// translation unit, which isn't desirable if
-// (in the non-ideal event that) it's not 1,
-// it will cause testNoExtraOxoolKitsLeft to
-// wait unnecessarily and fail.
-static int InitialOxoolKitCount = 1;
-static std::chrono::steady_clock::time_point TestStartTime;
-
-static void testCountHowManyOxoolkits()
+void helpers::testCountHowManyOxoolkits()
 {
     const char testname[] = "countHowManyOxoolkits ";
-    TestStartTime = std::chrono::steady_clock::now();
+    resetTestStartTime();
 
-    InitialOxoolKitCount = countOxoolKitProcesses(InitialOxoolKitCount);
+    countOxoolKitProcesses(InitialOxoolKitCount);
     TST_LOG("Initial oxoolkit count is " << InitialOxoolKitCount);
     LOK_ASSERT(InitialOxoolKitCount > 0);
 
-    TestStartTime = std::chrono::steady_clock::now();
+    resetTestStartTime();
 }
 
-static void testNoExtraOxoolKitsLeft()
+void helpers::testNoExtraOxoolKitsLeft()
 {
     const char testname[] = "noExtraOxoolKitsLeft ";
     const int countNow = countOxoolKitProcesses(InitialOxoolKitCount);
     LOK_ASSERT_EQUAL(InitialOxoolKitCount, countNow);
 
-    const auto duration = (std::chrono::steady_clock::now() - TestStartTime);
-    const auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    const auto durationMs = timeSinceTestStartMs();
 
     TST_LOG(" (" << durationMs << ')');
 }
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+void helpers::waitForKitProcessToStop(
+        const pid_t pid,
+        const std::string& testname,
+        const std::chrono::milliseconds timeoutMs /* = COMMAND_TIMEOUT_MS * 8 */,
+        const std::chrono::milliseconds retryMs /* = 10ms */)
+{
+    TST_LOG("Waiting for kit process " << pid << " to stop.");
+
+    std::set<pid_t> pids = getDocKitPids();
+    TST_LOG("Active kit pids are: " << getPidList(pids));
+
+    int tries = (timeoutMs / retryMs);
+    while(pids.contains(pid) && tries >= 0)
+    {
+        std::this_thread::sleep_for(retryMs);
+        pids = getDocKitPids();
+        tries--;
+    }
+
+    if (pids.contains(pid))
+    {
+        std::ostringstream oss;
+        oss << "Timed out waiting for kit process " << pid << " to stop. Active kit pids are: " << getPidList(pids);
+        LOK_ASSERT_FAIL(oss.str());
+    }
+    else
+    {
+        TST_LOG("Finished waiting for kit process " << pid << " to stop.");
+        TST_LOG("Active kit pids are: " << getPidList(pids));
+    }
+}
+
+std::string getPidList(std::set<pid_t> pids)
+{
+    std::ostringstream oss;
+    oss << "[";
+    for (pid_t i : pids)
+    {
+        oss << i << ", ";
+    }
+    oss << "]";
+    return oss.str();
+}

test/httpcrashtest.cpp
@@ -39,7 +39,7 @@
 #include <Protocol.hpp>
 #include <test.hpp>
 #include <helpers.hpp>
-#include <countoxoolkits.hpp>
+#include <KitPidHelpers.hpp>
 
 using namespace helpers;
 
@@ -153,7 +153,7 @@ void HTTPCrashTest::testCrashKit()
         TST_LOG("Killing oxoolkit instances.");
 
         killLoKitProcesses();
-        countOxoolKitProcesses(0, std::chrono::seconds(1));
+        countOxoolKitProcesses(0);//, std::chrono::seconds(1));
 
         TST_LOG("Reading the error code from the socket.");
         //FIXME: implement in WebSocketSession.
@@ -188,7 +188,7 @@ void HTTPCrashTest::testRecoverAfterKitCrash()
         TST_LOG("Killing oxoolkit instances.");
 
         killLoKitProcesses();
-        countOxoolKitProcesses(0, std::chrono::seconds(1));
+        countOxoolKitProcesses(0);//, std::chrono::seconds(1));
 
         // We expect the client connection to close.
         TST_LOG("Reconnect after kill.");
@@ -270,7 +270,6 @@ static void killPids(const std::set<pid_t> &pids, const std::string& testname)
 void HTTPCrashTest::killLoKitProcesses()
 {
     killPids(getKitPids(), "killLoKitProcesses ");
-    InitialOxoolKitCount = 1; // non-intuitive but it will arrive soon.
 }
 
 void HTTPCrashTest::killForkitProcess()

test/integration-http-server.cpp
@@ -17,7 +17,7 @@
 #include <Session.hpp>
 #include <Common.hpp>
 #include <common/FileUtil.hpp>
-#include <countcoolkits.hpp>
+#include <KitPidHelpers.hpp>
 
 #include <Poco/Net/AcceptCertificateHandler.h>
 #include <Poco/Net/FilePartSource.h>
@@ -99,14 +99,14 @@ public:
     void setUp()
     {
         helpers::resetTestStartTime();
-        testCountHowManyOxoolkits();
+        helpers::testCountHowManyOxoolkits();
         helpers::resetTestStartTime();
     }
 
     void tearDown()
     {
         helpers::resetTestStartTime();
-        testNoExtraOxoolKitsLeft();
+        helpers::testNoExtraOxoolKitsLeft();
         helpers::resetTestStartTime();
     }
 