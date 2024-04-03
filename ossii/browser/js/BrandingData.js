f6a8ef3cbe733c791fb9b79014732671c1381d05
Firefly <firefly@ossii.com.tw>
2024-04-03 10:46:47 +0800

    Separate branding data from main.js into BrandingData.js
    Signed-off-by: Firefly <firefly@ossii.com.tw>
    Change-Id: Ie2c62eaa34528ffa0e6af7e58462f5428dcf1650

---
+++ ossii/browser/js/BrandingData.js
@@ -0,0 +1,15 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+/**
+ * 這裏是品牌資訊，用來存放品牌名稱、產品名稱、產品網址、FAQ...
+ * 用來存放品牌相關資料，所有的品牌相關資料都應該放在這裏。
+ *
+ * 這個檔案應該在 main.js 之前載入。
+ */
+
+(function (global) {
+	global.brandProductName = 'OxOffice Online';
+	global.brandProductURL = 'https://ossii.com.tw';
+	global.brandProductFAQURL = 'https://www.facebook.com/OSSIITW/';
+})(window);
+
+/* vim: set ts=8 sts=8 sw=8 tw=100: */
--- ossii/browser/js/main.js
+++ ossii/browser/js/main.js
@@ -1,28 +1,22 @@
 /* -*- js-indent-level: 8; fill-column: 100 -*- */
 /* global L app */
 
-(function (global) {
-    global.brandProductName = 'OxOffice Online';
-    global.brandProductURL = 'https://ossii.com.tw';
-    global.brandProductFAQURL = 'https://www.facebook.com/OSSIITW/';
-})(window);
-
 (function() {
-    'use strict';
-    // Inititalize OxOOL
-    app.oxool = L.oxool({
-        map: app.map,
-    });
+	'use strict';
+	// Inititalize OxOOL
+	app.oxool = new L.OxOOL({
+		map: app.map,
+	});
 
-    // register the dialogs control
-    // TODO: 將來應該要移到 OxOOL 中
-    app.map.addControl(L.control.dialogs());
+	// register the dialogs control
+	// TODO: 將來應該要移到 OxOOL 中
+	app.map.addControl(L.control.dialogs());
 
-    // 紀錄 brandingjs 已經載入完畢
-    window.brandingjsLoaded = true;
+	// 紀錄 brandingjs 已經載入完畢
+	window.brandingjsLoaded = true;
 
-    // 觸發 branding:loaded 事件，通知其他元件，brandingjs 已經載入完畢
-    app.map.fire('branding:loaded');
+	// 觸發 branding:loaded 事件，通知其他元件，brandingjs 已經載入完畢
+	app.map.fire('branding:loaded');
 
 })();
 
