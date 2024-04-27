CollaboraOnline/online

ScrollSection: fix panning with RTL
Previously we did not take into account whether something was RTL when
scrolling it with ScrollSection. By introduing a new isRTL function to
check this, we can do something different (in this case scrolling the
other direction) if we are controlling an element that should be RTL.

We need to introduce a function, as whether we are using RTL can change
over the lifetime of the ScrollSection

Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
Change-Id: If4261a3e32375f6127241b846b97a3b4ac29eb0b

browser/src/layer/tile/ScrollSection.ts
@@ -22,7 +22,9 @@ export class ScrollSection extends CanvasSectionObject {
 	pendingScrollEvent: any = null;
 	stepByStepScrolling: boolean = false; // quick scroll will move "page up/down" not "jump to"
 
-	constructor () {
+	isRTL: () => boolean;
+
+	constructor (isRTL?: () => boolean) {
 		super({
 			name: L.CSections.Scroll.name,
 			anchor: [],
@@ -40,6 +42,8 @@ export class ScrollSection extends CanvasSectionObject {
 		this.sectionProperties = {};
 
 		this.map = L.Map.THIS;
+
+		this.isRTL = isRTL ?? (() => false);
 
 		this.map.on('scrollto', this.onScrollTo, this);
 		this.map.on('scrollby', this.onScrollBy, this);
@@ -702,6 +706,10 @@ export class ScrollSection extends CanvasSectionObject {
 	}
 
 	public scrollHorizontalWithOffset (offset: number): void {
+		if (this.isRTL()) {
+			offset = -offset;
+		}
+
 		var go = true;
 		if (offset > 0) {
 			if (this.documentTopLeft[0] + offset > this.sectionProperties.xMax)
@@ -1063,6 +1071,6 @@ export class ScrollSection extends CanvasSectionObject {
 
 }
 
-L.getNewScrollSection = function () {
-	return new cool.ScrollSection();
+L.getNewScrollSection = function (isRTL?: () => boolean) {
+	return new cool.ScrollSection(isRTL);
 };

OSSII/OXOOL

browser/src/layer/tile/ScrollSection.ts
@@ -22,7 +22,9 @@ export class ScrollSection extends CanvasSectionObject {
 	pendingScrollEvent: any = null;
 	stepByStepScrolling: boolean = false; // quick scroll will move "page up/down" not "jump to"
 
-	constructor () {
+	isRTL: () => boolean;
+
+	constructor (isRTL?: () => boolean) {
 		super({
 			name: L.CSections.Scroll.name,
 			anchor: [],
@@ -40,6 +42,8 @@ export class ScrollSection extends CanvasSectionObject {
 		this.sectionProperties = {};
 
 		this.map = L.Map.THIS;
+
+		this.isRTL = isRTL ?? (function () { return false; });
 
 		this.map.on('scrollto', this.onScrollTo, this);
 		this.map.on('scrollby', this.onScrollBy, this);
@@ -672,6 +676,10 @@ export class ScrollSection extends CanvasSectionObject {
 	}
 
 	public scrollHorizontalWithOffset (offset: number): void {
+		if (this.isRTL()) {
+			offset = -offset;
+		}
+
 		var go = true;
 		if (offset > 0) {
 			if (this.documentTopLeft[0] + offset > this.sectionProperties.xMax)
@@ -1033,6 +1041,6 @@ export class ScrollSection extends CanvasSectionObject {
 
 }
 
-L.getNewScrollSection = function () {
-	return new oxool.ScrollSection();
+L.getNewScrollSection = function (isRTL?: () => boolean) {
+	return new oxool.ScrollSection(isRTL);
 };