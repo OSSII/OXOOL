a9b8dc502427666ff9de499b4d4b774b09720fdd
Firefly <firefly@ossii.com.tw>
2024-06-05 17:33:51 +0800

    Add additional message file OtherMessages.js
    This file is only used to generate .po file message entries.
    There is no need to package it into the execution environment.
    
    Signed-off-by: Firefly <firefly@ossii.com.tw>
    Change-Id: Ib2284e96e92c1c14b306c90bf8051147d5a3fe09

---
+++ ossii/browser/js/OtherMessages.js
@@ -0,0 +1,49 @@
+/* -*- js-indent-level: 8 -*- */
+/* global _ */
+
+// OtherMessages.js
+// Only used for make l10n to generate .po files.
+// No need to pack.
+var OtherMessages = {
+	fromMenubar: [
+		_('PDF Document (.pdf)'), // PDF 文件(.pdf)
+		_('TEXT Document (.txt)'), // 純文字文件(.txt)
+		_('HTML Document (.html)'), // HTML 文件(.html)
+		_('ODF text document (.odt)'), // ODF 文字文件(.odt)
+		_('ODF spreadsheet (.ods)'), // ODF 試算表文件(.ods)
+		_('ODF presentation (.odp)'), // ODF 簡報文件(.odp)
+		_('Word 2003 Document (.doc)'), // Word 2003 文件(.doc)
+		_('Excel 2003 Spreadsheet (.xls)'), // Excel 2003 試算表(.xls)
+		_('PowerPoint 2003 Presentation (.ppt)'), // PowerPoint 2003 簡報(.ppt)
+		_('Word Document (.docx)'), // Word 文件(.docx)
+		_('Excel Spreadsheet (.xlsx)'), // Excel 試算表(.xlsx)
+		_('PowerPoint Presentation (.pptx)'), // PowerPoint 簡報(.pptx)
+		_('Rich Text (.rtf)'), // RTF 文件(.rtf)
+		_('CSV (.csv)'), // CSV 文件(.csv)
+		_('EPUB Document (.epub)'), // EPUB 電子書(.epub)
+		_('Remote image'), // 雲端圖片
+		_('Binding Line Menu'), // 裝訂線
+		_('Insert on the left side of page'), // 插入頁面左側
+		_('Insert on the right side of page'), // 插入頁面右側
+		_('Insert on the top side of page'), // 插入頁面上方
+		_('Insert on the bottom side of page'), // 插入頁面底部
+		_('Customize'), // 自訂
+		_('Tabbed view'), // 分頁式界面
+		_('Menu Bar'), // 選單列
+		_('Dark mode'), // 深色模式
+	],
+
+	notebookbar: [
+		_('File'), // 檔案
+		_('Home'), // 常用
+		_('Review'), // 校閱
+		_('Help'), // 說明
+		_('Theme'), // 佈景主題
+		_('Export As'), // 匯出爲
+		_('Rich Text'), // 文字
+		_('Checkbox'), // 核取方塊
+		_('Dropdown'), // 下拉式選項
+		_('Sparkline'), // 走勢圖
+		_('Page Number'), // 頁碼
+	],
+};
