CollaboraOnline/online

admin: handle case where monitor connection to controller is disconnected
- if monitor connection to doesn't exist because maybe controller died
or something we shouldn't wait for migration messages

Signed-off-by: Rashesh Padia <rashesh.padia@collabora.com>
Change-Id: I568f365f534e100010fe31ef6ae80f1b4c839cc2

wsd/Admin.cpp
@@ -30,7 +30,6 @@
 #include <Util.hpp>
 #include <common/JsonUtil.hpp>
 
-
 #include <net/Socket.hpp>
 #if ENABLE_SSL
 #include <SslSocket.hpp>
@@ -697,6 +696,25 @@ void Admin::pollingThread()
 
     if (!COOLWSD::IndirectionServerEnabled)
         return;
+
+    // if don't have monitor connection to the controller we set the _migrateMsgReceived
+    // for each docbroker so that docbroker can cleanup the documents
+    bool controllerMonitorConnection = false;
+    for (const auto& pair : _monitorSockets)
+    {
+        if (pair.first.find("controller") != std::string::npos)
+        {
+            controllerMonitorConnection = true;
+            break;
+        }
+    }
+
+    if (!controllerMonitorConnection)
+    {
+        LOG_WRN("Monitor connection to the controller doesn't exist, skipping shutdown migration");
+        COOLWSD::setAllMigrationMsgReceived();
+        return;
+    }
 
     _model.sendShutdownReceivedMsg();
 

OSSII/OXOOL

wsd/Admin.cpp
@@ -33,7 +33,6 @@
 #include <Util.hpp>
 #include <common/JsonUtil.hpp>
 
-
 #include <net/Socket.hpp>
 #if ENABLE_SSL
 #include <SslSocket.hpp>
@@ -714,6 +713,25 @@ void Admin::pollingThread()
 
     if (!OXOOLWSD::IndirectionServerEnabled)
         return;
+
+    // if don't have monitor connection to the controller we set the _migrateMsgReceived
+    // for each docbroker so that docbroker can cleanup the documents
+    bool controllerMonitorConnection = false;
+    for (const auto& pair : _monitorSockets)
+    {
+        if (pair.first.find("controller") != std::string::npos)
+        {
+            controllerMonitorConnection = true;
+            break;
+        }
+    }
+
+    if (!controllerMonitorConnection)
+    {
+        LOG_WRN("Monitor connection to the controller doesn't exist, skipping shutdown migration");
+        OXOOLWSD::setAllMigrationMsgReceived();
+        return;
+    }
 
     _model.sendShutdownReceivedMsg();
 